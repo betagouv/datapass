020eb212ffa3a79773e0b4f3bedd06bbbcc54e25
Pascal Knoth <pknoth@octo.com>
2018-06-05 17:50:09 +0200

    some

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ansible-playbook deploy-node.yml -i inventories/sandbox/hosts -e app_name=dgfip-contractualization-front -e app_version=sandbox
-ansible-playbook deploy-rails.yml -i inventories/sandbox/hosts -e app_name=dgfip-contractualization-back -e app_version=sandbox
-ansible-playbook deploy-rails.yml -i inventories/sandbox/hosts -e app_name=dgfip-contractualization-oauth -e app_version=sandbox
+ansible-playbook -i inventories/sandbox/hosts configure.yml
+ansible-playbook deploy-oauth.yml -i inventories/sandbox/hosts
+ansible-playbook deploy-back.yml -i inventories/sandbox/hosts
