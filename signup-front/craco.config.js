48b76bfd187d5f1d5885eb01470da190112fb873
Stanislas Bernard <bernardstanislas@gmail.com>
2021-09-16 18:14:41 +0200

    feature: Use esbuild within CRA scripts

---
+++ signup-front/craco.config.js
@@ -0,0 +1,38 @@
+const CracoEsbuildPlugin = require('craco-esbuild');
+const { ProvidePlugin } = require('webpack');
+
+module.exports = {
+  webpack: {
+    plugins: [
+      new ProvidePlugin({
+        React: 'react',
+      }),
+    ],
+  },
+  plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+      options: {
+        esbuildLoaderOptions: {
+          // Optional. Defaults to auto-detect loader.
+          loader: 'tsx', // Set the value to 'tsx' if you use typescript
+          target: 'es2015',
+        },
+        esbuildMinimizerOptions: {
+          // Optional. Defaults to:
+          target: 'es2015',
+          css: true, // if true, OptimizeCssAssetsWebpackPlugin will also be replaced by esbuild.
+        },
+        skipEsbuildJest: true, // Optional. Set to true if you want to use babel for jest tests,
+        esbuildJestOptions: {
+          loaders: {
+            '.js': 'ts',
+            '.jsx': 'tsx',
+            '.ts': 'ts',
+            '.tsx': 'tsx',
+          },
+        },
+      },
+    },
+  ],
+};
