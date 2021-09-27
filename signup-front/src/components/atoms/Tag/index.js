9db385845f7f43a2c145a16ebf8ea3af6ad3bef3
Raphaël Dubigny <rdubigny@gmail.com>
2021-09-27 12:05:45 +0200

    feat: display copy link as tag

--- signup-front/src/components/atoms/Tag/index.js
+++ signup-front/src/components/atoms/Tag/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './style.css';
 
-const Tag = ({ type = '', onClick, children, ...props }) => {
+const Tag = ({ type = '', onClick, href, children, ...props }) => {
   let className = `fr-tag`;
 
   if (type) {
     className += ` ${type}`;
+  }
+
+  if (!!href) {
+    return (
+      <a className={className} href={href} {...props}>
+        {children}
+      </a>
+    );
   }
 
   if (!!onClick) {
