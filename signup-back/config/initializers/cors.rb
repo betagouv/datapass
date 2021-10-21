5df65206e73e26157849e67136bcdb246c8a38df
Raphaël Dubigny <rdubigny@gmail.com>
2021-10-21 19:12:29 +0200

    chore: CORS are managed by rails instead of nginx (allow multiple origin)

---
+++ signup-back/config/initializers/cors.rb
@@ -0,0 +1,25 @@
+to_regexp = ->(string) {
+  Regexp.new(
+    "^#{Regexp.escape(string).gsub('\*', "[a-zA-Z0-9-]*")}$"
+  )
+}
+
+hosts = ENV.fetch("ALLOWED_ORIGINS").split(",").map(&to_regexp)
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(*hosts)
+
+    resource "*",
+      methods: %i[get post put patch delete options],
+      headers: %i[Authorization DNT User-Agent X-Requested-With If-Modified-Since Cache-Control Content-Type Range],
+      credentials: true,
+      max_age: 1728000
+  end
+
+  allow do
+    origins "*"
+
+    resource "/api/stats/*", methods: :get, headers: :any
+  end
+end
