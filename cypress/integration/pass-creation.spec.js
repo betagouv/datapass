92727df55fa31d0b91bd31a2d8515b3623e99d23
Stanislas Bernard <bernardstanislas@gmail.com>
2021-10-26 17:04:38 +0200

    test: Test login

--- cypress/integration/pass-creation.spec.js
+++ cypress/integration/pass-creation.spec.js
@@ -1,6 +1,14 @@
 describe("DataPass", () => {
-  it("lets the user create a pass", () => {
-    cy.visit("http://localhost:4000");
+  it("present the welcome page to an anonymous user", () => {
+    cy.visit("http://localhost:3000");
     cy.contains("Bienvenue sur DataPass !");
+  });
+
+  it("logs the user in and lets them submit a form", () => {
+    cy.visit("http://localhost:3001/users/auth/api_gouv?prompt=login"); // Directly visit the backend login page to avoid CORS issues with the cookies "Same-site: lax" policy
+
+    cy.get('input[name="login"]').type("user@yopmail.com");
+    cy.get('input[name="password"]').type("user@yopmail.com");
+    cy.get("form").submit();
   });
 });
