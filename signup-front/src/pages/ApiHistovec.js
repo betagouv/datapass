a3259fb6152c63361a77ae0d2965f8d3d1a57a57
etienne0101 <79003454+etienne0101@users.noreply.github.com>
2021-11-17 16:19:46 +0100

    create histovec

---
+++ signup-front/src/pages/ApiHistovec.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Form from '../components/templates/Form';
+import OrganisationSection from '../components/organisms/form-sections/OrganisationSection';
+import DescriptionSection from '../components/organisms/form-sections/DescriptionSection';
+import DonneesSection from '../components/organisms/form-sections/DonneesSection';
+import CadreJuridiqueSection from '../components/organisms/form-sections/CadreJuridiqueSection';
+import CguSection from '../components/organisms/form-sections/CguSection';
+import ÉquipeSection from '../components/organisms/form-sections/ÉquipeSection';
+import { DATA_PROVIDER_CONTACT_EMAILS } from '../config/data-provider-parameters';
+
+const DonneesDescription = () => (
+  <>
+    <p>
+      La loi informatique et libertés définit les principes à respecter lors de
+      la collecte, du traitement et de la conservation de données personnelles.
+    </p>
+    <p>L’article 6 précise :</p>
+    <ul>
+      <li>
+        3° [les données] sont adéquates, pertinentes et non excessives au regard
+        des finalités pour lesquelles elles sont collectées et de leurs
+        traitements ultérieurs ;
+      </li>
+      <li>
+        4° Elles sont exactes, complètes et, si nécessaire, mises à jour ; les
+        mesures appropriées doivent être prises pour que les données inexactes
+        ou incomplètes au regard des finalités pour lesquelles elles sont
+        collectées ou traitées soient effacées ou rectifiées ;
+      </li>
+    </ul>
+    <p>
+      Nous vous remercions de sélectionner uniquement les données strictement
+      nécessaires à votre téléservice. Le non-respect du principe de
+      proportionnalité vous expose vis-à-vis de la CNIL.
+    </p>
+  </>
+);
+
+const CadreJuridiqueDescription = () => (
+  <>
+    <p>
+      Pour pouvoir bénéficier du raccordement à l’API Particulier, le cadre
+      légal et réglementaire des fournisseurs de service doit permettre à la
+      DINUM de transmettre des données personnelles à votre entité
+      administrative.
+    </p>
+    <p>
+      Dans le cas où vous représentez une collectivité, veuillez joindre la
+      délibération du conseil municipal explicitant l’usage des données
+      demandées.
+    </p>
+    <p>
+      Important : à priori, vous n’avez pas de nouvelle délibération à réaliser.
+      Il vous suffit de déposer la dernière délibération tarifaire qui détaille
+      les barèmes de facturation des services, ou le règlement qui décrit les
+      données nécessaires à une instruction.
+    </p>
+    <p>
+      Pour en savoir plus, consultez{' '}
+      <a href="https://api.gouv.fr/guides/deliberation-api-part">
+        notre guide sur ce qu’est une bonne délibération
+      </a>
+      .
+    </p>
+  </>
+);
+
+const availableScopes = [
+  {
+    value: 'info_vehicule_histovec',
+    label: 'Informations sur le véhicule',
+    mandatory: true,
+  },
+  {
+    value: 'historique_vehicule_histovec',
+    label: 'Historique du véhicule',
+    mandatory: true,
+  },
+];
+
+const target_api = 'api_histovec';
+
+const ApiHistovec = ({
+  match: {
+    params: { enrollmentId },
+  },
+}) => (
+  <Form
+    enrollmentId={enrollmentId}
+    target_api={target_api}
+    contactInformation={[
+      {
+        email: DATA_PROVIDER_CONTACT_EMAILS.api_particulier,
+        label: 'Nous contacter',
+        subject: 'Contact%20via%20datapass.api.gouv.fr',
+      },
+    ]}
+  >
+    <OrganisationSection />
+    <DescriptionSection />
+    <DonneesSection
+      availableScopes={availableScopes}
+      DonneesDescription={DonneesDescription}
+    />
+    <CadreJuridiqueSection
+      CadreJuridiqueDescription={CadreJuridiqueDescription}
+    />
+    <ÉquipeSection />
+    <CguSection cguLink="https://api.gouv.fr/resources/CGU%20API%20Particulier.pdf" />
+  </Form>
+);
+
+ApiHistovec.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      enrollmentId: PropTypes.string,
+    }),
+  }),
+};
+
+ApiHistovec.defaultProps = {
+  match: {
+    params: {
+      enrollmentId: null,
+    },
+  },
+};
+
+export default ApiHistovec;
