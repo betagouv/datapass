cc81b38b442b696b9b61ec5bbcb52a032b1118fe
etienne0101 <79003454+etienne0101@users.noreply.github.com>
2021-12-15 17:37:54 +0100

    INGRES back

--- signup-back/app/policies/enrollment/api_ingres_policy.rb
+++ signup-back/app/policies/enrollment/api_ingres_policy.rb
@@ -1,9 +1,10 @@
-class Enrollment::ApiIngresNoyauPolicy < EnrollmentPolicy
+class Enrollment::ApiIngresPolicy < EnrollmentPolicy
   def permitted_attributes
     res = super
 
     res.concat([
       scopes: [
+        :donnees_ingres_nomenclatures,
         :donnees_ingres_noyau
       ]
     ])
