08a51c1a1a3a6006ba93b4c7e7c0d9da6f3c00de
Raphaël Dubigny <rdubigny@gmail.com>
2022-01-13 12:41:16 +0100

    fix: test fails at matomo initialization

--- signup-front/src/App.tsx
+++ signup-front/src/App.tsx
@@ -14,9 +14,10 @@ import Alert from './components/atoms/Alert';
 import { useEffect } from 'react';
 
 const urlBase: string = process.env.REACT_APP_PIWIK_URL || 'https://matomo.org';
+// 1 is default value for no siteId since matomo considers 0 as an empty id and does not accept null value
 const siteId: number =
-  parseInt(process.env.REACT_APP_PIWIK_SITE_ID as string) || 0;
-const disabled: boolean = !urlBase || !siteId;
+  parseInt(process.env.REACT_APP_PIWIK_SITE_ID as string) || 1;
+const disabled: boolean = !urlBase || siteId === 1;
 
 const instance = createInstance({
   urlBase,
