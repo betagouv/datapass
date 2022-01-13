24922b9ab71f13c726892f477e617813aadff739
SchweisguthN <n.schweisguth@gmail.com>
2022-01-13 15:06:26 +0100

    Création formulaire Hubee DILA

---
+++ signup-front/src/pages/HubeePortailDila.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Form from '../components/templates/Form';
+import OrganisationSection from '../components/organisms/form-sections/OrganisationSection';
+import DemarcheEnLigneSection from '../components/organisms/form-sections/hubee-sections/DemarcheEnLigneSection';
+import ÉquipeSection from '../components/organisms/form-sections/ÉquipeSection';
+import CguSection from '../components/organisms/form-sections/CguSection';
+import { DATA_PROVIDER_PARAMETERS } from '../config/data-provider-parameters';
+import IntituleInitializerSection from '../components/organisms/form-sections/hubee-sections/IntituleInitializerSection';
+
+const initialContacts = {
+  demandeur: {
+    header: 'Demandeur',
+    description: (
+      <>
+        <b>Le demandeur</b>, c’est vous, dépose la demande d’abonnement.
+      </>
+    ),
+    forceDisable: true,
+  },
+  responsable_metier: {
+    header: 'Responsable d’abonnement',
+    description: (
+      <>
+        <b>Le responsable d’abonnement</b> disposera des droits d’administration
+        sur le portail HubEE : gestion des abonnements et gestion des
+        utilisateurs.
+      </>
+    ),
+  },
+  responsable_traitement: null,
+  delegue_protection_donnees: null,
+  responsable_technique: null,
+};
+
+const target_api = 'hubee_portail_dila';
+
+const HubeePortailDila = () => (
+  <Form
+    target_api={target_api}
+    contactEmail={DATA_PROVIDER_PARAMETERS[target_api]?.email}
+    documentationUrl="https://www.numerique.gouv.fr/dinum/"
+  >
+    <OrganisationSection />
+    <IntituleInitializerSection value="Abonnement au portail HubEE" />
+    <DemarcheEnLigneSection />
+    <ÉquipeSection initialContacts={initialContacts} />
+    <CguSection cguLink="/docs/20210212_dinum_hubee_cgu_v2_1_0_version_site.pdf" />
+  </Form>
+);
+
+export default HubeePortailDila;
