b8394b6474d094b1d5ad5576fb7d413153d7ebac
Raphaël Dubigny <rdubigny@gmail.com>
2022-01-17 17:32:25 +0100

    chore: upgrade sentry client

---
+++ signup-front/src/components/organisms/ErrorBoundaryFallback.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Alert from '../atoms/Alert';
+import IndexPointingRightEmoji from '../atoms/icons/IndexPointingRightEmoji';
+
+export const ErrorBoundaryFallback = () => (
+  <div className="page">
+    <main className="full-page">
+      <Alert type="error" title="Erreur inconnue">
+        <p>
+          Une erreur est survenue. Merci de réessayer ultérieurement. Vous
+          pouvez également essayer d'utiliser un autre navigateur (ex: Firefox
+          ou Chrome).
+        </p>
+        <p>
+          Si cette erreur persiste, signalez nous cette erreur à l’adresse{' '}
+          <a href="mailto:contact@api.gouv.fr?subject=Erreur%20sur%20datapass.api.gouv.fr">
+            contact@api.gouv.fr
+          </a>
+          .
+        </p>
+        <br />
+        <p>
+          <IndexPointingRightEmoji />
+          {' '}
+          <a href="/">Retour à l’accueil</a>
+        </p>
+      </Alert>
+    </main>
+  </div>
+);
+
+export default ErrorBoundaryFallback;
--- signup-front/src/index.tsx
+++ signup-front/src/index.tsx
@@ -3,14 +3,12 @@ import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
 import App from './App';
 import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import 'moment/locale/fr'; // set moment locale to french globally
+import Sentry from '@sentry/react';
 
-import Raven from 'raven-js';
-
-// Setup sentry
 if (process.env.NODE_ENV === 'production') {
-  Raven.config(
-    'https://7ccf44097ef645f8835408b9d73e7781@sentry.data.gouv.fr/8'
-  ).install();
+  Sentry.init({
+    dsn: 'https://7ccf44097ef645f8835408b9d73e7781@sentry.data.gouv.fr/8',
+  });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
