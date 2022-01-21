1603cece8bb1b5112b6089dc13d7837fab9b03c0
Raphaël Dubigny <rdubigny@gmail.com>
2022-01-21 15:28:33 +0100

    feat: add validation for technical team

--- signup-back/app/models/enrollment/api_entreprise.rb
+++ signup-back/app/models/enrollment/api_entreprise.rb
@@ -4,6 +4,7 @@ class Enrollment::ApiEntreprise < Enrollment
   def submit_validation
     super
 
+    technical_team_validation
     scopes_validation
     team_members_validation("responsable_technique", "contact technique")
     team_members_validation("contact_metier", "contact métier")
