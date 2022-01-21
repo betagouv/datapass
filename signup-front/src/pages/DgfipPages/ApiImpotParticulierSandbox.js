50098b9e9331d0d1460b969883eb62c651b1768e
Raphaël Dubigny <rdubigny@gmail.com>
2022-01-21 15:53:07 +0100

    fix: ConfirmNewDemarcheId modal is triggerd on first selection

--- signup-front/src/pages/DgfipPages/ApiImpotParticulierSandbox.js
+++ signup-front/src/pages/DgfipPages/ApiImpotParticulierSandbox.js
@@ -17,7 +17,7 @@ import { DATA_PROVIDER_PARAMETERS } from '../../config/data-provider-parameters'
 import PreviousEnrollmentSection from '../../components/organisms/form-sections/PreviousEnrollmentSection';
 
 const demarches = {
-  ...fcAvailableDemarches,
+  ...JSON.parse(JSON.stringify(fcAvailableDemarches)),
   eligibilite_lep: {
     label: 'Vérification de l’éligibilité au Livret d’épargne populaire (LEP)',
     state: {
