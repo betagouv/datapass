a8705a488111435f7ef8059a3a29294b5a92abd8
Raphaël Dubigny <rdubigny@gmail.com>
2022-01-25 18:57:45 +0100

    fix: update attachment location path

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -2,6 +2,6 @@ CarrierWave.configure do |config|
   config.root = if Rails.env.test?
     Rails.root.join("tmp")
   else
-    Rails.root.join("/opt/apps/signup-back")
+    Rails.root.join("/opt/apps/datapass-backend")
   end
 end
