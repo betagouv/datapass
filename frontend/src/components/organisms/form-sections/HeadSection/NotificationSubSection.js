cf73b5567b76b8cb2ee05d24c68da917a2644d87
Raphaël Dubigny <rdubigny@gmail.com>
2022-02-09 18:38:14 +0100

    WIP

--- frontend/src/components/organisms/form-sections/HeadSection/NotificationSubSection.js
+++ frontend/src/components/organisms/form-sections/HeadSection/NotificationSubSection.js
@@ -32,6 +32,17 @@ export const NotificationSubSection = () => {
               Pensez à enregistrer régulièrement vos modifications.
             </Alert>
           )}
+          <Alert type="warning">
+            Votre organisation a déjà une habilitation à une ou plusieurs
+            démarches de la DILA :
+            <span>
+              {' '}
+              <a href={`/authorization-request/1234`}>l'habilitation #7421</a>
+            </span>
+            . Merci de la consulter avant de remplir ce formulaire : toute
+            habilitation demandée en doublon entraînera le refus de votre
+            requête.
+          </Alert>
         </>
       )}
     </>
