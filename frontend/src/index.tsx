83cddc5a74d8510a04e33a8fcdf798f2506bb845
Raphaël Dubigny <rdubigny@gmail.com>
2022-02-16 11:35:55 +0100

    chore: use react axe in dev env

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
 import App from './App';
@@ -8,6 +9,15 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: 'https://7ccf44097ef645f8835408b9d73e7781@sentry.data.gouv.fr/8',
   });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  try {
+    const axe = require('@axe-core/react');
+    axe(React, ReactDOM, 1000);
+  } catch {
+    // ignoring silently
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
