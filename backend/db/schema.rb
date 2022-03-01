bb519cd6771873a8c2b4e95ce98ba5eb37a41912
Raphaël Dubigny <rdubigny@gmail.com>
2022-03-01 19:20:08 +0100

    fix: CERTDC is stored as a string

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_105227) do
+ActiveRecord::Schema.define(version: 2022_03_01_173145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
