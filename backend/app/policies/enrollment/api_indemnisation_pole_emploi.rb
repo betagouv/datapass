7f262ec5fa81045ef507b06f2a145b8a40c44b0d
Etienne0101 <etiennepichotdamon@gmail.com>
2022-03-11 11:09:25 +0100

    create indemnisation pole emploi

---
+++ backend/app/policies/enrollment/api_indemnisation_pole_emploi.rb
@@ -0,0 +1,13 @@
+class Enrollment::ApiStatutDemandeurEmploiPolicy < EnrollmentPolicy
+    def permitted_attributes
+      res = super
+  
+      res.concat([
+        scopes: [
+          :'api_fc-statutaugmentev1'
+        ]
+      ])
+  
+      res
+    end
+  end
