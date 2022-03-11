8013c0987d2eb1e1b71fcd65e9bb64eeb56e1eff
Etienne0101 <etiennepichotdamon@gmail.com>
2022-03-11 11:28:43 +0100

    Update api_indemnisation_pole_emploi.rb

--- backend/app/models/enrollment/api_indemnisation_pole_emploi.rb
+++ backend/app/models/enrollment/api_indemnisation_pole_emploi.rb
@@ -3,7 +3,7 @@ class Enrollment::ApiIndemnisationPoleEmploi < Enrollment
 
   def submit_validation
     super
-  
+
     scopes_validation
     responsable_technique_validation
     previous_enrollment_id_validation
