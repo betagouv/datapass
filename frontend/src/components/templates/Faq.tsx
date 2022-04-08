84b14bf69ee9d893770d84202190dfdad0af610f
Nicolas Schweisguth <n.schweisguth@gmail.com>
2022-04-08 10:41:01 +0200

    Update Faq.tsx

--- frontend/src/components/templates/Faq.tsx
+++ frontend/src/components/templates/Faq.tsx
@@ -6,7 +6,7 @@ import ExpandableQuote from '../molecules/ExpandableQuote';
 const questions = [
   {
     anchor: 'probleme-habilitation',
-    title: 'J’ai une question concernant mon habilitation ?',
+    title: 'J’ai une question concernant mon habilitation.',
     body: (
       <>
         <p>
@@ -18,8 +18,93 @@ const questions = [
     ),
   },
   {
+    anchor: 'changement-contact',
+    title: 'Comment puis-je modifier les coordonnées de mon compte ?',
+    body: (
+      <>
+        Si vous souhaitez modifier les coordonnées de votre compte (par exemple
+        si cela vous a été demandé dans le cadre d'une demande d'habilitation),{' '}
+        <Link
+          newTab
+          inline
+          href="https://auth.api.gouv.fr/users/personal-information"
+        >
+          rendez-vous ici.
+        </Link>
+        <br />
+        <br />A noter : Vous ne pouvez pas modifier l'adresse mail associée au
+        compte. Si vous souhaitez utiliser une autre adresse mail, merci de{' '}
+        <Link inline href="https://auth-staging.api.gouv.fr/users/sign-in">
+          créer un nouveau compte.
+        </Link>
+      </>
+    ),
+  },
+  {
+    anchor: 'ajout-siret',
+    title:
+      'Puis-je rejoindre une autre organisation que celle à laquelle je suis rattaché(e) ?',
+    body: (
+      <>
+        Tout à fait, vous pouvez{' '}
+        <Link inline href="https://auth.api.gouv.fr/users/join-organization">
+          le faire ici.
+        </Link>
+        <br />
+        <br />
+        Si vous avez plusieurs organisations associées à votre compte, vous
+        pouvez choisir celui pour lequel vous formulez une demande
+        d'habilitation dans le cadre "Vous faites cette demande pour".
+      </>
+    ),
+  },
+  {
+    anchor: 'organisation-etrangere',
+    title:
+      'Puis-je demander une habilitation pour une organisation étrangère ?',
+    body: (
+      <>
+        Non, Datapass ne permet de demander des habilitations juridiques que
+        pour les organisations enregistrées auprès de l'INSEE.
+      </>
+    ),
+  },
+  {
+    anchor: 'champ-editeur',
+    title: 'Comment répondre à la question "Qui va implémenter l’API" ?',
+    body: (
+      <>
+        Sur quelques formulaires d'habilitation, la question "Qui va implémenter
+        l’API" vous est posée. Cette question permet au fournisseur de l'API de
+        vous accompagner au mieux dans son utilisation. 3 options sont possibles
+        :
+        <br />
+        <br />- Développeurs en interne : les personnes qui vont implémenter
+        l'API travaillent dans la même organisation que vous (ou c’est
+        vous-même).
+        <br />- Editeur de logiciel : vous utilisez un logiciel, et c'est via
+        cet outil que vous allez exploiter les données.
+        <br />- Autre : si les deux options ci-dessus ne vous conviennent pas,
+        vous pouvez indiquer le nom ou le SIRET de l'organisation qui
+        implémentera l'API pour vous.
+      </>
+    ),
+  },
+  {
+    anchor: 'liste-editeur',
+    title:
+      'Que faire si mon éditeur de logiciel n’est pas dans la liste proposée ?',
+    body: (
+      <>
+        Vous pouvez taper le nom de votre éditeur directement, même s'il n'est
+        pas dans la liste des éditeurs qui vous est proposée : l'information
+        sera prise en compte.
+      </>
+    ),
+  },
+  {
     anchor: 'contact',
-    title: 'Je ne trouve pas la réponse à ma question ?',
+    title: 'Je ne trouve pas la réponse à ma question.',
     body: (
       <>
         Vous pouvez{' '}
