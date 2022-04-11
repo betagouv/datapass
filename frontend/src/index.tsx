486b301e23c36651e773de3f67b1e563cf980c72
Raphaël Dubigny <rdubigny@gmail.com>
2022-04-11 16:04:53 +0200

    chore: follow migration guide to react 18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
 import App from './App';
 import 'moment/locale/fr'; // set moment locale to french globally
@@ -20,4 +21,6 @@ if (process.env.NODE_ENV !== 'production') {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(<App />);
