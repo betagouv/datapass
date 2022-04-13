34ec0dce9a0efa9c5b247d2d6a3bfeeb9ccfed9f
Etienne0101 <etiennepichotdamon@gmail.com>
2022-04-13 15:59:28 +0200

    Update ApiDeclarationEmbauche.js

--- frontend/src/pages/UrssafPages/ApiDeclarationEmbauche.js
+++ frontend/src/pages/UrssafPages/ApiDeclarationEmbauche.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Form from '../../components/templates/Form';
 import OrganisationSection from '../../components/organisms/form-sections/OrganisationSection';
+import { DATA_PROVIDER_PARAMETERS } from '../../config/data-provider-parameters';
+
 
 const target_api = 'api_declaration_embauche';
 
