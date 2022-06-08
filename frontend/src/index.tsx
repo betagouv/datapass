c357edcfb9bc6f25bddcbe57a40217c471357eb9
Isabelle Lafont <isalafont@gmail.com>
2022-06-08 17:41:21 +0200

    Ignore 401 errors in Sentry
    See Sentry Docs https://docs.sentry.io/platforms/javascript/guides/react/configuration/filtering/
    Find this solution from https://stackoverflow.com/questions/61651440/prevent-sentry-io-from-logging-server-error-codes
    
    Regex flag mi standing for:
    'm': multiline mode
    'i': case insensitive
    More Details about Regex Flag here : https://javascript.info/regexp-introduction

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,17 @@
+import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
+import * as Sentry from '@sentry/react';
+import 'moment/locale/fr'; // set moment locale to french globally
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import '@gouvfr/dsfr/dist/dsfr/dsfr.css';
 import App from './App';
-import 'moment/locale/fr'; // set moment locale to french globally
-import * as Sentry from '@sentry/react';
 
 if (process.env.NODE_ENV === 'production') {
+  const serverErrorsRegex = new RegExp(`401 Unauthorized`, 'mi');
+
   Sentry.init({
     dsn: 'https://7ccf44097ef645f8835408b9d73e7781@sentry.data.gouv.fr/8',
+    ignoreErrors: [serverErrorsRegex],
   });
 }
 
