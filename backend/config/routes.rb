0f97ff3cedd93d8a97d2c2b3157b0d6dc5784b4f
Raphaël Dubigny <rdubigny@gmail.com>
2022-06-13 13:23:26 +0200

    feat: add ping route for insee services

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
       constraints: {capability_url_id: /[A-Za-z0-9]{64}/}
 
     get "/insee/etablissements/:siret", to: "insee#etablissements", siret: /\d{14}/
+    get "/insee/etablissements/ping", to: "insee#ping"
     get "/insee/codes_naf/:id", to: "insee#codes_naf", id: /\d{2}\.\d{2}[A-Z]/
     get "/insee/categories_juridiques/:id", to: "insee#categories_juridiques", id: /[0-9]{1,4}/
   end
