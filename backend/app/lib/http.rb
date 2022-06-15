661517e454fb78c21267830a3b593c1ecbb72639
Raphaël Dubigny <rdubigny@gmail.com>
2022-06-15 17:02:03 +0200

    feat: centralise login into stdout
    note that in production env stdout is redirected to
    /var/log/datapass-backend/access.log by systemd

--- backend/app/lib/http.rb
+++ backend/app/lib/http.rb
@@ -4,8 +4,7 @@ class Http
   include Singleton
 
   def get(url_as_string, api_key, endpoint_label, auth_header = nil, auth_method = "Bearer")
-    logger = Logger.new($stdout)
-    http = HTTP.timeout(10).use(logging: {logger: logger})
+    http = HTTP.timeout(10).use(logging: {logger: Rails.logger})
 
     response = if auth_header.nil?
       http
@@ -46,8 +45,7 @@ class Http
   end
 
   def request(http_verb, url_as_string, body, api_key, endpoint_label, auth_header, auth_method, content_type)
-    logger = Logger.new($stdout)
-    http = HTTP.timeout(10).use(logging: {logger: logger})
+    http = HTTP.timeout(10).use(logging: {logger: Rails.logger})
       .headers(accept: "application/json")
 
     http_with_auth = if auth_header.nil?
