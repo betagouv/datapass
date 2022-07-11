cdd2ab3bccac2f1d0df15e9f3187839479a27e6b
Isabelle Lafont <isalafont@gmail.com>
2022-07-11 15:54:28 +0200

    add modification

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -50,7 +50,9 @@ module DataPass
 
     config.action_mailer.perform_deliveries = true
 
-    config.action_mailer.default_url_options = {host: ENV["BACK_HOST"]}
+    # remove scheme from url
+    uri = URI(ENV["BACK_HOST"])
+    config.action_mailer.default_url_options = {host: uri.hostname + uri.path}
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address: "smtp-relay.sendinblue.com",
