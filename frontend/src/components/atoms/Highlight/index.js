f2c371bb23e65098ff60b2f1501860f8a00953fb
Isabelle Lafont <isalafont@gmail.com>
2022-07-20 14:32:04 +0200

    Clean css

--- frontend/src/components/atoms/Highlight/index.js
+++ frontend/src/components/atoms/Highlight/index.js
@@ -2,16 +2,13 @@ import Button from '../hyperTexts/Button';
 import './style.css';
 
 export const Highlight = ({ title, children, onClick }) => (
-  <div
-    className="fr-container--fluid
-fr-mt-5w"
-  >
+  <div className="fr-container--fluid fr-mt-5w">
     <div className="fr-highlight">
       <p className="fr-h5 fr-mb-3w">{title}</p>
       <p className="fr-text--md">{children}</p>
     </div>
     <div className="fr-container--fluid">
-      <div className="fr-mt-5w">
+      <div>
         <Button outline icon="edit" onClick={onClick}>
           Régider un message
         </Button>
