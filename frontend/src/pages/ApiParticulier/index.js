125f8a98f2b3f69369d27bde7138511fa3d3db8b
Nicolas Schweisguth <n.schweisguth@gmail.com>
2022-08-04 10:14:55 +0200

    Update index.js

--- frontend/src/pages/ApiParticulier/index.js
+++ frontend/src/pages/ApiParticulier/index.js
@@ -48,9 +48,9 @@ const CadreJuridiqueDescription = () => (
       administrative. Merci de le compléter.
     </p>
     <p>
-      Dans le cas où vous représentez une collectivité, veuillez joindre la
+      Dans le cas où vous représenteriez une collectivité, veuillez joindre la
       délibération du conseil municipal explicitant l’usage des données
-      demandées. Viser l'article 114-8 du code des relations entre le public et
+      demandées. Citer l'article 114-8 du code des relations entre le public et
       l'administration ne peut pas se substituer à la présence d’une
       délibération du conseil municipal.
     </p>
