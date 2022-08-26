41168450c572097c2dfd0da1658c80d9a55e2416
Isabelle Lafont <isalafont@gmail.com>
2022-08-26 11:12:56 +0200

    Hide alert when click onAlertClose

--- frontend/src/components/templates/UserEnrollmentList/index.js
+++ frontend/src/components/templates/UserEnrollmentList/index.js
@@ -11,6 +11,7 @@ import { NewEnrollmentButton } from '../../molecules/NewEnrollmentButton';
 const UserEnrollmentList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [enrollmentsByOrganization, setEnrollmentsByOrganization] = useState();
+  const [showAlert, setShowAlert] = useState(false);
 
   const { goToItem } = useListItemNavigation();
 
@@ -27,6 +28,10 @@ const UserEnrollmentList = () => {
 
     onFetchData();
   }, []);
+
+  const handleClose = () => {
+    setShowAlert(!showAlert);
+  };
 
   return (
     <main className="list-page">
@@ -48,14 +53,16 @@ const UserEnrollmentList = () => {
 
       {!isLoading && !isEmpty(enrollmentsByOrganization) && (
         <div className="page-container list-container">
-          <Alert
-            type="success"
-            title="Votre message a bien été envoyé"
-            onAlertClose={() => null}
-          >
-            Vous allez recevoir une notification dans votre boite mail au moment
-            ou il sera traité.
-          </Alert>
+          {!showAlert && (
+            <Alert
+              type="success"
+              title="Votre message a bien été envoyé"
+              onAlertClose={handleClose}
+            >
+              Vous allez recevoir une notification dans votre boite mail au
+              moment ou il sera traité.
+            </Alert>
+          )}
           {Object.keys(enrollmentsByOrganization).map((group) => (
             <React.Fragment key={group}>
               <div className="list-title fr-text--lead">
