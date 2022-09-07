21597fe73a1e99ca09f9d43ae2bfa612560a791b
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-07 10:47:00 +0200

    test: introduce EnrollmentsController#export basic tests

---
+++ backend/spec/controllers/enrollments/export_spec.rb
@@ -0,0 +1,49 @@
+require "csv"
+
+RSpec.describe EnrollmentsController, "#export", type: :controller do
+  describe "authorization" do
+    subject do
+      get :export
+    end
+
+    context "without user" do
+      it { is_expected.to have_http_status(:unauthorized) }
+    end
+
+    context "with user" do
+      let(:user) { create(:user) }
+
+      before do
+        login(user)
+      end
+
+      it { is_expected.to have_http_status(:ok) }
+    end
+  end
+
+  describe "payload" do
+    subject do
+      get :export
+      response.body
+    end
+
+    let(:user) { create(:instructor, target_api: "franceconnect") }
+    let!(:enrollment) { create(:enrollment, :franceconnect) }
+    let!(:foreign_enrollment) { create(:enrollment, :api_entreprise) }
+
+    before do
+      login(user)
+    end
+
+    it "is a valid csv, only on target apis" do
+      csv = CSV.parse(subject, headers: true)
+
+      expect(csv.count).to eq(1)
+
+      first_entry = csv.first
+
+      expect(first_entry["id"]).to eq(enrollment.id.to_s)
+      expect(first_entry["target_api"]).to eq("franceconnect")
+    end
+  end
+end
