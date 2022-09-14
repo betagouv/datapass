6fbfeef56975e0411792e4910c85f41a3b6e55a6
Raphaël Dubigny <rdubigny@gmail.com>
2022-09-14 11:34:39 +0200

    feat: remove auto redirect

--- frontend/src/components/organisms/FormRouter.js
+++ frontend/src/components/organisms/FormRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { DATA_PROVIDER_PARAMETERS } from '../../config/data-provider-parameters';
-import useListItemNavigation from '../templates/hooks/use-list-item-navigation';
 import { AuthRequired } from './AuthContext';
 import NotFound from './NotFound';
 
@@ -10,11 +9,8 @@ const FormRouter = () => {
 
   const TargetApiComponent =
     DATA_PROVIDER_PARAMETERS[targetApi.replace(/-/g, '_')]?.component;
-  const { goBackToList } = useListItemNavigation();
 
   if (!TargetApiComponent) {
-    setTimeout(() => goBackToList(), 3000);
-
     return <NotFound />;
   }
 
