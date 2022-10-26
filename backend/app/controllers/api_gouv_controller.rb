828b02e56a2f54350d4291d4ddf0a8360020896c
Isabelle Lafont <isalafont@gmail.com>
2022-10-26 14:42:48 +0200

    create api_gouv controller with method apis_list

--- backend/app/controllers/api_gouv_controller.rb
+++ backend/app/controllers/api_gouv_controller.rb
@@ -1,8 +1,14 @@
 class ApiGouvController < ApplicationController
-  # GET /api_gouv/title
+  # GET /api_gouv/apis_list
   def apis_list
-    # Call apis_list
+    apis_list = ApiGouv.call
+
+    if apis_list.nil?
+      return render status: :not_found, json: {apis_list: nil}
+    end
+
     render status: :ok, json: {
+      apis_list: apis_list
     }
   end
 end
