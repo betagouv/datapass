41866f2abb39e9a3a4e52d018456aa70a41d778e
Isabelle Lafont <isalafont@gmail.com>
2022-11-03 18:01:58 +0100

    remove console log

--- frontend/src/components/templates/DataProviderList/DataProviderListApi.tsx
+++ frontend/src/components/templates/DataProviderList/DataProviderListApi.tsx
@@ -34,10 +34,8 @@ export const DataProviderListApi = () => {
       setResult(response.data);
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        console.log('error message: ', error.message);
         return error.message;
       } else {
-        console.log('unexpected error: ', error);
         return 'An unexpected error occurred';
       }
     }
