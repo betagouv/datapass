ce8bd6dcf93ee0eb2a1d4d6d11bfeeb1c8ae3fad
Alexis Leclerc <alexis@alxs.fr>
2022-11-04 14:22:08 +0100

    Add exception for public data

--- frontend/src/pages/ApiEntreprise/index.js
+++ frontend/src/pages/ApiEntreprise/index.js
@@ -233,6 +233,11 @@ const CadreJuridiqueDescription = () => (
       transmettre le décret ou l’arrêté justifiant votre demande.
     </p>
     <p>
+      La loi Lemaire (pour une république numérique) du 7 octobre 2016 suffit si
+      vous demandez des données publiques ; c‘est à dire les données n‘ayant pas
+      de 🔐 devant leur nom.
+    </p>
+    <p>
       <WarningEmoji /> Attention, quel que soit votre statut, le{' '}
       <Link
         inline
