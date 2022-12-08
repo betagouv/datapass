2558ec6b56d6b6a3a7bebb41efb02d5fa7be93bc
Isabelle Lafont <isalafont@gmail.com>
2022-12-08 15:38:35 +0100

    Refacto method names and files Change sidekiq config to perform ScheduleReminderEmailForDraftEnrollmentsWorker every day at 8:00 am Add test to check taht sidekiq scheduler got correct cronjob syntax, job classes and job names add link in sidekiq test

---
+++ backend/app/workers/extract_draft_enrollments_to_remind.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class ExtractDraftEnrollmentsToRemind
+  attr_reader :enrollment
+
+  def call
+    if created_at_updated_at_last_events?
+      created_at_updated_at_last_events_ids =
+        draft_enrollments_with_last_update_or_create_events.pluck(:enrollment_id).flatten
+      Enrollment.find(created_at_updated_at_last_events_ids)
+    end
+  end
+
+  def draft_enrollments
+    Enrollment.where(status: "draft")
+      .where({updated_at: (Time.now.beginning_of_day - 1.months)..Time.now.end_of_day - 15.days})
+      .includes(:events)
+      .where.not({events: {name: %w[notify]}})
+      .order(:id, "events.created_at")
+  end
+
+  def draft_enrollments_with_last_update_or_create_events
+    enrollments = draft_enrollments
+    events = enrollments.map { |enrollment| enrollment.events.last }.to_a
+    events.reject { |event| event.name == "reminder" }
+  end
+
+  def created_at_updated_at_last_events?
+    draft_enrollments_with_last_update_or_create_events.any?
+  end
+end
--- backend/spec/workers/extract_draft_enrollment_to_remind_spec.rb
+++ backend/spec/workers/extract_draft_enrollment_to_remind_spec.rb
@@ -2,8 +2,8 @@
 
 require "rails_helper"
 
-RSpec.describe ReminderEmailFinder, type: :worker do
-  subject { described_class.new(enrollments) }
+RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
+  subject { described_class.new }
 
   before do
     Timecop.freeze
@@ -52,17 +52,15 @@ RSpec.describe ReminderEmailFinder, type: :worker do
     Timecop.return
   end
 
-  let!(:enrollments) { Enrollment.all }
-
   context "#draft_enrollments" do
     it "renders draft enrollments between 15 days and a month ago" do
-      result = subject.draft_enrollments(enrollments)
+      result = subject.draft_enrollments
 
       expect(result.count).to eq(5)
     end
 
     it "includes draft enrollment with no notify events" do
-      result = subject.draft_enrollments(enrollments)
+      result = subject.draft_enrollments
       events = result.map { |enrollment| enrollment.events.map(&:name) }.flatten
 
       expect(events).to include("create", "update", "reminder")
@@ -70,7 +68,7 @@ RSpec.describe ReminderEmailFinder, type: :worker do
     end
 
     it "orders enrollment by id and last events created" do
-      result = subject.draft_enrollments(enrollments)
+      result = subject.draft_enrollments
       event_names = result.collect { |e| e.events.map(&:name) }
       event_ids = result.collect { |e| e.events.ids }
 
@@ -81,7 +79,7 @@ RSpec.describe ReminderEmailFinder, type: :worker do
 
   context "#draft_with_last_events_updated_at_or_created_at" do
     it "renders draft enrollment with update_at or created_at as last events" do
-      result = subject.draft_enrollment_last_events_updated_at_or_created_at(enrollments)
+      result = subject.draft_enrollments_with_last_update_or_create_events
       events = result.map { |event| event.name }.flatten
 
       expect(result.count).to eq(4)
@@ -94,52 +92,6 @@ RSpec.describe ReminderEmailFinder, type: :worker do
       result = subject.created_at_updated_at_last_events?
 
       expect(result).to be_truthy
-    end
-  end
-
-  describe "#create_reminder_event" do
-    it "create an event reminder if email is sent to demandeur" do
-      event_reminder = subject.call
-      enrollment = event_reminder.first
-      last_enrollment_event = enrollment.events.last
-
-      expect(last_enrollment_event.name).to eq("reminder")
-      expect(last_enrollment_event.created_at).to eq(Time.now)
-    end
-  end
-
-  describe "email sends to demandeurs when enrollment is in draft for more than 15 days" do
-    include ActiveJob::TestHelper
-
-    after do
-      clear_enqueued_jobs
-      clear_performed_jobs
-    end
-
-    context "when enrollment is in draft for 15 days" do
-      # describe "non-regression test" do
-      #   it "enqueues an EmailNotifierMethods#reminder_draft_enrollment_email which does not raise an error" do
-      #     Sidekiq::Testing.inline!
-      #
-      #     expect {
-      #       perform_enqueued_jobs do
-      #         subject.call
-      #       end
-      #     }.not_to raise_error
-      #
-      #     Sidekiq::Testing.fake!
-      #   end
-      # end
-
-      it "sends email to demandeurs" do
-        subject.call
-
-        enqueued_jobs = ActiveJob::Base.queue_adapter.enqueued_jobs
-        reminder_email = enqueued_jobs.find { |job| job["arguments"][1] == "reminder_draft_enrollment_email" }
-
-        expect(ActiveJob::Base.queue_adapter.enqueued_jobs.size).to eq 4
-        expect(reminder_email).to be_truthy
-      end
     end
   end
 end
