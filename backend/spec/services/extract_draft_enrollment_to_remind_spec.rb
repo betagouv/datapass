7eb40c193dd85514fada74ddeb6e195c6c145dbe
Isabelle Lafont <isalafont@gmail.com>
2022-12-15 17:26:57 +0100

    correct anti pattern in enrollment_reminder_email

--- backend/spec/services/extract_draft_enrollment_to_remind_spec.rb
+++ backend/spec/services/extract_draft_enrollment_to_remind_spec.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
-RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
+RSpec.describe ExtractDraftEnrollmentsToRemind, type: :service do
   subject { described_class.new }
 
   context "enrollment not included in #draft_enrollments call" do
-    before :each do
+    before do
       Timecop.freeze
-
-      enrollment = create(:enrollment, :hubee_portail, :draft, created_at: 94.days.ago, updated_at: 50.days.ago)
-      create(:event, :create, enrollment: enrollment, created_at: 94.days.ago, updated_at: 94.days.ago)
-      create(:event, :update, enrollment: enrollment, created_at: 50.days.ago, updated_at: 50.days.ago)
     end
 
-    after :each do
+    let!(:enrollment) { create(:enrollment, :hubee_portail, :draft, created_at: 94.days.ago, updated_at: 50.days.ago) }
+    let!(:event) { create(:event, :create, enrollment: enrollment, created_at: 94.days.ago, updated_at: 94.days.ago) }
+    let!(:event_1) { create(:event, :update, enrollment: enrollment, created_at: 50.days.ago, updated_at: 50.days.ago) }
+
+    after do
       Timecop.return
     end
 
@@ -27,14 +25,17 @@ RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
   end
 
   context "enrollment created in the last 14 days" do
-    before :each do
+    before do
       Timecop.freeze
-
-      enrollment = create(:enrollment, :api_entreprise, :draft, created_at: 5.days.ago, updated_at: 5.days.ago)
-      create(:event, :create, enrollment: enrollment, created_at: 5.days.ago, updated_at: 5.days.ago)
     end
 
-    after :each do
+    let!(:api_entreprise) { create(:enrollment, :api_entreprise, :draft, created_at: 15.days.ago, updated_at: 14.days.ago) }
+    let!(:event_api_entreprise) { create(:event, :create, enrollment: api_entreprise, created_at: 15.days.ago, updated_at: 14.days.ago) }
+
+    let!(:api_part) { create(:enrollment, :api_particulier, :draft, created_at: 5.days.ago, updated_at: 5.days.ago) }
+    let!(:event_api_part) { create(:event, :create, enrollment: api_part, created_at: 5.days.ago, updated_at: 5.days.ago) }
+
+    after do
       Timecop.return
     end
 
@@ -48,26 +49,26 @@ RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
 
   context "#draft_enrollments" do
     context "when it includes only draft enrollments" do
-      before :each do
+      before do
         Timecop.freeze
-
-        enrollment = create(:enrollment, :api_entreprise, :validated, created_at: 35.days.ago, updated_at: 30.days.ago)
-        create(:event, :create, enrollment: enrollment, created_at: 35.days.ago, updated_at: 35.days.ago)
-        create(:event, :update, enrollment: enrollment, created_at: 30.days.ago, updated_at: 30.days.ago)
-        create(:event, :validate, enrollment: enrollment, created_at: 30.days.ago, updated_at: 30.days.ago)
-
-        enrollment = create(:enrollment, :franceconnect, :draft, created_at: 60.days.ago, updated_at: 15.days.ago)
-        create(:event, :create, enrollment: enrollment, created_at: 60.days.ago, updated_at: 60.days.ago)
-        create(:event, :update, enrollment: enrollment, created_at: 30.days.ago, updated_at: 30.days.ago)
-        create(:event, :reminder, enrollment: enrollment, created_at: 15.days.ago, updated_at: 15.days.ago)
-
-        enrollment = create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago)
-        create(:event, :create, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-        create(:event, :notify, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-        create(:event, :update, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
       end
 
-      after :each do
+      let!(:api_entreprise) { create(:enrollment, :api_entreprise, :validated, created_at: 35.days.ago, updated_at: 30.days.ago) }
+      let!(:event_api_entreprise_1) { create(:event, :create, enrollment: api_entreprise, created_at: 35.days.ago, updated_at: 35.days.ago) }
+      let!(:event_api_entreprise_2) { create(:event, :update, enrollment: api_entreprise, created_at: 30.days.ago, updated_at: 30.days.ago) }
+      let!(:event_api_entreprise_3) { create(:event, :validate, enrollment: api_entreprise, created_at: 30.days.ago, updated_at: 30.days.ago) }
+
+      let!(:franceconnect) { create(:enrollment, :franceconnect, :draft, created_at: 60.days.ago, updated_at: 15.days.ago) }
+      let!(:event_franceconnect) { create(:event, :create, enrollment: franceconnect, created_at: 60.days.ago, updated_at: 60.days.ago) }
+      let!(:event_franceconnect_1) { create(:event, :update, enrollment: franceconnect, created_at: 30.days.ago, updated_at: 30.days.ago) }
+      let!(:event_franceconnect_2) { create(:event, :reminder, enrollment: franceconnect, created_at: 15.days.ago, updated_at: 15.days.ago) }
+
+      let!(:api_impot_particulier_fc_sandbox) { create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot) { create(:event, :create, enrollment: api_impot_particulier_fc_sandbox, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot_1) { create(:event, :notify, enrollment: api_impot_particulier_fc_sandbox, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot_2) { create(:event, :update, enrollment: api_impot_particulier_fc_sandbox, created_at: 16.days.ago, updated_at: 16.days.ago) }
+
+      after do
         Timecop.return
       end
 
@@ -77,72 +78,72 @@ RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
         expect(result.count).to eq(2)
       end
     end
-  end
 
-  context "checking enrollment's events" do
-    before :each do
-      Timecop.freeze
+    context "checking enrollment's events" do
+      before :all do
+        Timecop.freeze
+      end
 
-      enrollment = create(:enrollment, :franceconnect, :draft, created_at: 60.days.ago, updated_at: 15.days.ago)
-      create(:event, :create, enrollment: enrollment, created_at: 60.days.ago, updated_at: 60.days.ago)
-      create(:event, :update, enrollment: enrollment, created_at: 30.days.ago, updated_at: 30.days.ago)
-      create(:event, :reminder, enrollment: enrollment, created_at: 15.days.ago, updated_at: 15.days.ago)
+      let!(:franceconnect) { create(:enrollment, :franceconnect, :draft, created_at: 60.days.ago, updated_at: 15.days.ago) }
+      let!(:event_franceconnect) { create(:event, :create, enrollment: franceconnect, created_at: 60.days.ago, updated_at: 60.days.ago) }
+      let!(:event_franceconnect_1) { create(:event, :update, enrollment: franceconnect, created_at: 30.days.ago, updated_at: 30.days.ago) }
+      let!(:event_franceconnect_2) { create(:event, :reminder, enrollment: franceconnect, created_at: 15.days.ago, updated_at: 15.days.ago) }
 
-      enrollment = create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :create, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :notify, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :update, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
+      let!(:impot_particulier) { create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot_part) { create(:event, :create, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot_part_1) { create(:event, :notify, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
+      let!(:event_impot_part_2) { create(:event, :update, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
 
-      enrollment = create(:enrollment, :franceconnect, :draft, created_at: 15.days.ago, updated_at: 15.days.ago)
-      create(:event, name: "create", enrollment: enrollment, created_at: 15.days.ago, updated_at: 15.days.ago)
-    end
+      let!(:hubee_portail) { create(:enrollment, :hubee_portail, :draft, created_at: 16.days.ago, updated_at: 15.days.ago) }
+      let!(:event_hubee_portail) { create(:event, :create, enrollment: hubee_portail, created_at: 15.days.ago, updated_at: 15.days.ago) }
 
-    after :each do
-      Timecop.return
-    end
+      after :all do
+        Timecop.return
+      end
 
-    it "includes draft enrollment with no notify events" do
-      result = subject.draft_enrollments
-      events = result.map { |enrollment| enrollment.events.map(&:name) }.flatten
+      it "includes draft enrollment with no notify events" do
+        result = subject.draft_enrollments
+        events = result.map { |enrollment| enrollment.events.map(&:name) }.flatten
 
-      expect(events).to include("create", "update", "reminder")
-      expect(events).not_to include("notify")
-    end
+        expect(events).to include("create", "update", "reminder")
+        expect(events).not_to include("notify")
+      end
 
-    it "orders enrollment by id and last events created" do
-      result = subject.draft_enrollments
-      event_names = result.collect { |e| e.events.map(&:name) }
-      event_ids = result.collect { |e| e.events.ids }
+      it "orders enrollment by id and last events created" do
+        result = subject.draft_enrollments
+        event_names = result.collect { |e| e.events.map(&:name) }
+        event_ids = result.collect { |e| e.events.ids }
 
-      expect(event_names[0].last).to eq("reminder")
-      expect(event_ids[0].last).to eq(result[0].events.last.id)
-    end
+        expect(event_names[0].last).to eq("reminder")
+        expect(event_ids[0].last).to eq(result[0].events.last.id)
+      end
 
-    context "#last_update_or_create_events_for_draft_enrollments" do
-      it "renders update_at or created_at as last events" do
-        result = subject.last_update_or_create_events_for_draft_enrollments
-        events = result.map { |event| event.name }.flatten
+      context "#last_update_or_create_events_for_draft_enrollments" do
+        it "renders update_at or created_at as last events" do
+          result = subject.last_update_or_create_events_for_draft_enrollments
+          events = result.map { |event| event.name }.flatten
 
-        expect(result.count).to eq(2)
-        expect(events).to include("create", "update")
+          expect(result.count).to eq(2)
+          expect(events).to include("create", "update")
+        end
       end
     end
   end
 
   context "when #call checks if there is any enrollments" do
-    before :each do
+    before :all do
       Timecop.freeze
-
-      enrollment = create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :create, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :notify, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-      create(:event, :update, enrollment: enrollment, created_at: 16.days.ago, updated_at: 16.days.ago)
-
-      enrollment = create(:enrollment, :franceconnect, :draft, created_at: 15.days.ago, updated_at: 15.days.ago)
-      create(:event, name: "create", enrollment: enrollment, created_at: 15.days.ago, updated_at: 15.days.ago)
     end
 
-    after :each do
+    let!(:impot_particulier) { create(:enrollment, :api_impot_particulier_fc_sandbox, :draft, created_at: 16.days.ago, updated_at: 16.days.ago) }
+    let!(:event_impot) { create(:event, :create, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
+    let!(:event_impot_1) { create(:event, :notify, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
+    let!(:event_impot_2) { create(:event, :update, enrollment: impot_particulier, created_at: 16.days.ago, updated_at: 16.days.ago) }
+
+    let!(:franceconnect) { create(:enrollment, :franceconnect, :draft, created_at: 15.days.ago, updated_at: 15.days.ago) }
+    let!(:event) { create(:event, name: "create", enrollment: franceconnect, created_at: 15.days.ago, updated_at: 15.days.ago) }
+
+    after :all do
       Timecop.return
     end
 
@@ -160,6 +161,13 @@ RSpec.describe ExtractDraftEnrollmentsToRemind, type: :worker do
 
       enrollments = Enrollment.find(result_ids)
       expect(enrollments).to eq([Enrollment.find(result_ids[0]), Enrollment.find(result_ids[1])])
+    end
+
+    it "#call : returns an array" do
+      results = subject.call
+
+      expect(results).to be_an_instance_of(Array)
+      expect(results.count).to eq(2)
     end
   end
 end
