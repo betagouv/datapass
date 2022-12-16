48b7424d69381e5e7dad259254dc8fcf244da397
Isabelle Lafont <isalafont@gmail.com>
2022-12-16 14:16:33 +0100

    remove experimentalSessionAndOrigin as it's not require anymore

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,8 @@ module.exports = {
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      return require("./cypress/plugins/index.js")(on, config);
     },
   },
 };
