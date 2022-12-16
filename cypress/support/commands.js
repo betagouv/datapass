4b155e1a6cd4bce65523b3b088ddc1f12450a7b2
Isabelle Lafont <isalafont@gmail.com>
2022-12-16 13:16:25 +0100

    fix test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,17 @@
 Cypress.Commands.add("login", (email, password) => {
   cy.session([email, password], () => {
-    cy.visit("http://localhost:3001/users/auth/api_gouv?prompt=login", {
-      method: "POST",
-    }); // Directly visit the backend login page to avoid CORS issues with the cookies "Same-site: lax" policy
+    cy.visit(
+      "http://localhost:3001/users/auth/api_gouv?prompt=login",
+      {
+        method: "POST",
+      }
+    ); // Directly visit the backend login page to avoid CORS issues with the cookies "Same-site: lax" policy
 
     cy.get('input[name="login"]').type(email);
     cy.get("form").submit();
     cy.get('input[name="password"]').type(password);
     cy.get('[action="/users/sign-in"]').submit();
+    cy.setCookie("_session_id", "123key");
   });
 });
 
@@ -15,6 +19,9 @@ Cypress.Commands.add("checkBox", (checkbox) => {
   cy.get(`label[for*="${checkbox}"]`).click({ position: "left" });
 });
 
-Cypress.Commands.add("fillField", (fieldName, value, fieldType = "input") => {
-  cy.get(`${fieldType}[name="${fieldName}"]`).clear().type(value);
-});
+Cypress.Commands.add(
+  "fillField",
+  (fieldName, value, fieldType = "input") => {
+    cy.get(`${fieldType}[name="${fieldName}"]`).clear().type(value);
+  }
+);
