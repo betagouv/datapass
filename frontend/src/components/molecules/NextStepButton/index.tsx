6f6a59af2ef7515d2f3fe2cd7543286469f49236
Raphaël Dubigny <rdubigny@gmail.com>
2022-12-21 17:30:10 +0100

    refactor: use useDataProvider hook

--- frontend/src/components/molecules/NextStepButton/index.tsx
+++ frontend/src/components/molecules/NextStepButton/index.tsx
@@ -1,6 +1,6 @@
 import { EnrollmentStatus } from '../../../config/status-parameters';
 import Button from '../../atoms/hyperTexts/Button';
-import { useDataProviderConfigurations } from '../../templates/hooks/use-data-provider-configurations';
+import { useDataProvider } from '../../templates/hooks/use-data-provider';
 
 const NextStepButton = ({
   target_api,
@@ -9,19 +9,18 @@ const NextStepButton = ({
   target_api: string;
   status: EnrollmentStatus;
 }) => {
-  const { dataProviderConfigurations } = useDataProviderConfigurations();
+  const { url, type } = useDataProvider(target_api);
 
-  const configuration = dataProviderConfigurations?.[target_api];
-  if (status === 'validated' && configuration?.url) {
-    if (configuration.type === 'api') {
+  if (status === 'validated' && url) {
+    if (type === 'api') {
       return (
-        <Button secondary large href={configuration.url}>
+        <Button secondary large href={url}>
           Récupérer mon jeton
         </Button>
       );
-    } else if (configuration.type === 'service') {
+    } else if (type === 'service') {
       return (
-        <Button secondary large href={configuration.url}>
+        <Button secondary large href={url}>
           Accéder au service
         </Button>
       );
