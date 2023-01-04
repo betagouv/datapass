fc3c8836ad6fae339ca3336d0361a26e22f260dc
Pierre-Eric Garcia <garciapierreeric@gmail.com>
2023-01-04 12:35:10 +0100

    Simplify user header when he's connected

--- frontend/src/components/atoms/icons/fr-fi-icons.tsx
+++ frontend/src/components/atoms/icons/fr-fi-icons.tsx
@@ -4,6 +4,7 @@ import '@gouvfr/dsfr/dist/utility/icons/icons-business/icons-business.min.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-design/icons-design.min.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-document/icons-document.min.css';
 import '@gouvfr/dsfr/dist/utility/icons/icons-device/icons-device.min.css';
+import '@gouvfr/dsfr/dist/utility/icons/icons-buildings/icons-buildings.min.css';
 
 const FrFiIcon: React.FC<{
   color?: string;
