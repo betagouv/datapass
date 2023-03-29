3f095b09ea8d98b293c8a7b30eabbb9f8c367d02
Isabelle Lafont <isalafont@gmail.com>
2023-03-29 18:59:11 +0200

    re add error message if scope not checked

--- backend/app/models/enrollment/hubee_portail.rb
+++ backend/app/models/enrollment/hubee_portail.rb
@@ -7,5 +7,9 @@ class Enrollment::HubeePortail < Enrollment
     errors.add(:dpo_is_informed, :invalid, message: "Vous devez confirmer avoir informé le DPD de votre organisation avant de continuer") unless dpo_is_informed?
     team_members_validation("responsable_metier", "administrateur métier")
     no_hubee_certdc_validation
+
+    if scopes.empty?
+      errors.add(:scopes, :invalid, message: "Vous devez cocher au moins une démarche en ligne avant de continuer")
+    end
   end
 end
