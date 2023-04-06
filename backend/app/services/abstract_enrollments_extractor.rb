314a502dc142459b600bfb11bb3e66d7124835a8
Isabelle Lafont <isalafont@gmail.com>
2023-04-06 18:17:17 +0200

    second refacto of abstract_enrollments_extractor

--- backend/app/services/abstract_enrollments_extractor.rb
+++ backend/app/services/abstract_enrollments_extractor.rb
@@ -10,7 +10,8 @@ class AbstractEnrollmentsExtractor
   end
 
   def call
-    filter_enrollments
+    enrollments = query_enrollments
+    filter_enrollments(enrollments)
   end
 
   def query_enrollments
@@ -24,27 +25,30 @@ class AbstractEnrollmentsExtractor
       })
   end
 
-  def filter_enrollments
-    enrollments = query_enrollments
-    most_recent_event_of_each_enrollment = enrollments.lazy.map do |enrollment|
+  def map_enrollments_to_last_events(enrollments)
+    enrollments.map do |enrollment|
       sorted_events = enrollment.events.sort_by(&:created_at)
       sorted_events.last
     end
+  end
 
-    enrollment_ids = most_recent_event_of_each_enrollment
-      .to_a
-      .select do |event|
+  def filter_events_by_extract_criteria(events)
+    events.select do |event|
       extract_criteria[:most_recent_event_names].include?(event.name) &&
         (
           !extract_criteria[:time_since_most_recent_event] ||
-          event.created_at.between?(
-            extract_from_date.beginning_of_day,
-            extract_criteria[:time_since_most_recent_event].ago.end_of_day
-          )
+            event.created_at.between?(
+              extract_from_date.beginning_of_day,
+              extract_criteria[:time_since_most_recent_event].ago.end_of_day
+            )
         )
     end
-      .pluck(:enrollment_id)
+  end
 
-    Enrollment.find(enrollment_ids)
+  def filter_enrollments(enrollments)
+    enrollments_last_events = map_enrollments_to_last_events(enrollments)
+    filtered_last_events_by_extract_criteria = filter_events_by_extract_criteria(enrollments_last_events)
+    enrollments_ids = filtered_last_events_by_extract_criteria.pluck(:enrollment_id)
+    Enrollment.find(enrollments_ids)
   end
 end
