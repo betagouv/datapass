ac4e4773ff381f5915c08da50aa4db8c2527f3e7
Nicolas Schweisguth <n.schweisguth@gmail.com>
2023-05-10 11:13:55 +0200

    Ajout du fichier de policy de Ficoba

---
+++ backend/app/policies/enrollment/api_ficoba_unique_policy.rb
@@ -0,0 +1,25 @@
+class Enrollment::ApiFicobaUniquePolicy < Enrollment::UniquePolicy
+  def permitted_attributes
+    res = super
+
+    res.concat([
+      scopes: [
+        :dgfip_ficoba_etat_civil_denomination,
+        :dgfip_ficoba_adresse,
+        :dgfip_ficoba_compte,
+        :dgfip_ficoba_etablissement_bancaire,
+        :dgfip_ficoba_date
+      ],
+      additional_content: [
+        :rgpd_general_agreement,
+        :acces_ficoba_iban,
+        :acces_ficoba_spi,
+        :acces_ficoba_siren,
+        :acces_ficoba_personne_physique,
+        :acces_ficoba_personne_morale
+      ]
+    ])
+
+    res
+  end
+end
