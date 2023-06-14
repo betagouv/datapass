85a7ad74f4efdbccd3f223400349551ddca529cb
Loïc Delmaire <loic.delmaire@gmail.com>
2023-06-14 14:36:00 +0200

    Rewrite/fix migration

--- backend/db/migrate/20230608080932_rename_api_entreprise_scopes_in_enrollments.rb
+++ backend/db/migrate/20230608080932_rename_api_entreprise_scopes_in_enrollments.rb
@@ -1,12 +1,14 @@
 class RenameApiEntrepriseScopesInEnrollments < ActiveRecord::Migration[7.0]
   def up
-    Enrollment.find_each do |enrollment|
+    valid_enrollments.find_each do |enrollment|
       scopes = enrollment.scopes.map do |scope|
         case scope
         when "associations"
           "open_data"
         when "associations_donnees_protegees"
           "associations_djepva"
+        when "attestations_agefiph"
+          "attestations_agefiph"
         when "attestations_fiscales"
           "attestation_fiscale_dgfip"
         when "attestations_sociales"
@@ -20,7 +22,7 @@ class RenameApiEntrepriseScopesInEnrollments < ActiveRecord::Migration[7.0]
         when "msa_cotisations"
           "cotisations_msa"
         when "certificat_opqibi"
-          "certification_opqibi"
+          "open_data"
         when "probtp"
           "cotisations_probtp"
         when "qualibat"
@@ -34,9 +36,9 @@ class RenameApiEntrepriseScopesInEnrollments < ActiveRecord::Migration[7.0]
         when "extraits_rcs"
           "open_data"
         when "entreprises"
-          "mandataires_sociaux_infogreffe"
+          ["open_data", "mandataires_sociaux_infogreffe", "unites_legales_etablissements_insee"]
         when "etablissements"
-          "open_data" && "unites_legales_etablissements_insee"
+          ["open_data", "unites_legales_etablissements_insee"]
         when "liasse_fiscale"
           "liasses_fiscales_dgfip"
         when "actes_inpi"
@@ -45,6 +47,8 @@ class RenameApiEntrepriseScopesInEnrollments < ActiveRecord::Migration[7.0]
           "comptes_annuels_inpi"
         when "conventions_collectives"
           "open_data"
+        when "effectifs_acoss"
+          "effectifs_urssaf"
         when "entreprises_artisanales"
           "open_data"
         when "eori_douanes"
@@ -56,48 +60,83 @@ class RenameApiEntrepriseScopesInEnrollments < ActiveRecord::Migration[7.0]
         end
       end
 
-      Enrollment.where(target_api: "api_entreprise").update_all(scopes: scopes)
+      scopes << "open_data"
+
+      begin
+        enrollment.update!(scopes: scopes.flatten.compact.uniq)
+      rescue ActiveRecord::RecordInvalid
+        print "Enrollment #{enrollment.id} could not be updated: #{enrollment.errors.full_messages}\n"
+      end
     end
   end
 
   def down
-    Enrollment.find_each do |enrollment|
+    valid_enrollments.find_each do |enrollment|
       scopes = enrollment.scopes.map do |scope|
         case scope
+        when "unites_legales_etablissements_insee"
+          ["entreprises", "etablissements"]
         when "associations_djepva"
           "associations_donnees_protegees"
+        when "mandataires_sociaux_infogreffe"
+          "entreprises"
+        when "chiffre_affaires_dgfip"
+          "exercices"
+        when "comptes_annuels_inpi"
+          "bilans_inpi"
+        when "bilans_bdf"
+          "bilans_entreprise_bdf"
+        when "liasses_fiscales_dgfip"
+          "liasse_fiscale"
         when "attestation_fiscale_dgfip"
           "attestations_fiscales"
         when "attestation_sociale_urssaf"
           "attestations_sociales"
-        when "bilans_bdf"
-          "bilans_entreprise_bdf"
-        when "certification_cnetp"
-          "certificat_cnetp"
         when "cotisations_msa"
           "msa_cotisations"
-        when "certification_opqibi"
-          "certificat_opqibi"
         when "cotisations_probtp"
           "probtp"
-        when "chiffre_affaires_dgfip"
-          "exercices"
-        when "mandataires_sociaux_infogreffe"
-          "entreprises"
-        when "open_data" && "unites_legales_etablissements_insee"
-          "etablissements"
-        when "liasses_fiscales_dgfip"
-          "liasse_fiscale"
-        when "comptes_annuels_inpi"
-          "bilans_inpi"
+        when "certification_cnetp"
+          "certificat_cnetp"
         when "open_data"
-          "associations" && "fntp_carte_pro" && "qualibat" && "certificat_rge_ademe" && "extrait_court_inpi" && "extraits_rcs" && "actes_inpi" && "conventions_collectives" && "entreprises_artisanales" && "eori_douanes" && "certificat_agence_bio"
+          [
+            "associations",
+            "fntp_carte_pro",
+            "qualibat",
+            "certificat_rge_ademe",
+            "extrait_court_inpi",
+            "extraits_rcs",
+            "actes_inpi",
+            "conventions_collectives",
+            "entreprises_artisanales",
+            "eori_douanes",
+            "certificat_agence_bio",
+            "certificat_opqibi"
+          ]
+        when "attestations_agefiph"
+          "attestations_agefiph"
         else
           scope
         end
       end
 
-      Enrollment.where(target_api: "api_entreprise").update_all(scopes: scopes)
+      begin
+        enrollment.update!(scopes: scopes.flatten.compact.uniq)
+      rescue ActiveRecord::RecordInvalid
+        print "Enrollment #{enrollment.id} could not be updated: #{enrollment.errors.full_messages}\n"
+      end
     end
+  end
+
+  private
+
+  def valid_enrollments
+    Enrollment.where(target_api: "api_entreprise").where("scopes is not null and id not in (?)", enrollement_without_organization_ids)
+  end
+
+  def enrollement_without_organization_ids
+    [
+      10794
+    ]
   end
 end
