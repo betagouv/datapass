7c1068b4e222b099a935907905d2b2075f387e42
Pierre-Eric Garcia <garciapierreeric@gmil.com>
2023-08-17 15:09:23 +0200

    Add another check when accessing target_api label

--- frontend/src/components/organisms/InstructorEnrollmentListFilters/index.tsx
+++ frontend/src/components/organisms/InstructorEnrollmentListFilters/index.tsx
@@ -59,7 +59,7 @@ const InstructorEnrollmentListFilters = ({
                 return {
                   key: targetApiKey,
                   label: dataProviderConfigurations?.[targetApiKey]
-                    .label as string,
+                    ?.label as string,
                 };
               }) || []
           }
