c3f2f3734b41cf0092606647f42d57893eb8e0d4
Pierre-Eric Garcia <garciapierreeric@gmil.com>
2023-08-18 09:40:55 +0200

    Started typescript migration for lib files

--- frontend/src/lib/http-client.ts
+++ frontend/src/lib/http-client.ts
@@ -7,27 +7,27 @@ const { REACT_APP_BACK_HOST: BACK_HOST } = process.env;
 axios.defaults.adapter = httpAdapter;
 
 axios.interceptors.request.use((config) => {
-  if (
-    new URL(config.url).origin === BACK_HOST &&
-    !new URL(config.url).pathname.startsWith('/api/stats')
-  ) {
-    config.withCredentials = true;
+  if (config.url) {
+    const requestURL = new URL(config.url);
+    if (
+      requestURL.origin === BACK_HOST &&
+      !requestURL.pathname.startsWith('/api/stats')
+    ) {
+      config.withCredentials = true;
+    }
   }
-
   return config;
 });
 
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (
-      error.response?.status === 401 &&
-      new URL(error.config.url).origin === BACK_HOST
-    ) {
-      // This is bad. Find out why in this function doc!
-      resetAuthContext();
+    if (error.config && error.config.url) {
+      const errorURL = new URL(error.config.url);
+      if (error.response?.status === 401 && errorURL.origin === BACK_HOST) {
+        resetAuthContext();
+      }
     }
-
     return Promise.reject(error);
   }
 );
