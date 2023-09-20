320486a0ca223c77989cf3f66681ef1116223800
Nicolas Schweisguth <n.schweisguth@gmail.com>
2023-09-20 17:40:22 +0200

    Changement des libellés du bloc Homologation

--- backend/app/models/concerns/dgfip_validation_methods.rb
+++ backend/app/models/concerns/dgfip_validation_methods.rb
@@ -20,12 +20,12 @@ module DgfipValidationMethods
     cadre_juridique_validation
 
     # Homologation de securite
-    errors.add(:additional_content, :invalid, message: "Vous devez renseigner le nom de l’autorité d’homologation avant de continuer") unless additional_content&.fetch("autorite_homologation_nom", false)&.present?
-    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la fonction de l’autorité d’homologation avant de continuer") unless additional_content&.fetch("autorite_homologation_fonction", false)&.present?
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner le nom de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer") unless additional_content&.fetch("autorite_homologation_nom", false)&.present?
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la fonction de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer") unless additional_content&.fetch("autorite_homologation_fonction", false)&.present?
     date_regex = /^\d{4}-\d{2}-\d{2}$/
-    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de début de l’homologation au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_homologation", ""))
-    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de fin de l’homologation au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_fin_homologation", ""))
-    errors.add(:documents, :invalid, message: "Vous devez joindre le document de décision d’homologation avant de continuer") unless documents.where(type: "Document::DecisionHomologation").present?
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de début de l’homologation ou de signature du questionnaire de sécurité au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_homologation", ""))
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de fin de l’homologation ou de signature du questionnaire de sécurité au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_fin_homologation", ""))
+    errors.add(:documents, :invalid, message: "Vous devez joindre le document de décision d’homologation ou le questionnaire de sécurité avant de continuer") unless documents.where(type: "Document::DecisionHomologation").present?
 
     # Volumétrie
     errors.add(:additional_content, :invalid, message: "Vous devez renseigner la limitation d’appels par minute avant de continuer") unless additional_content&.fetch("volumetrie_appels_par_minute", false)&.present?
