b245b2630bdc2b0ad47cfe70477b6bc5dd2f6517
Pierre-Eric Garcia <garciapierreeric@gmil.com>
2023-09-20 17:35:27 +0200

    Remove uncreated scopes

--- backend/app/models/enrollment/api_infinoe_unique.rb
+++ backend/app/models/enrollment/api_infinoe_unique.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Enrollment::ApiInfinoeUnique < Enrollment::UniqueEnrollment
+  include DgfipValidationMethods
 end
--- backend/app/policies/enrollment/api_infinoe_unique_policy.rb
+++ backend/app/policies/enrollment/api_infinoe_unique_policy.rb
@@ -1,14 +1,3 @@
 class Enrollment::ApiInfinoeUniquePolicy < Enrollment::UniquePolicy
   include DgfipPolicyMethods
-
-  def permitted_attributes
-    res = super
-
-    res.concat([
-      scopes: infinoe_permitted_scopes
-    ])
-    augment_permitted_attributes(res, :additional_content, *infinoe_permitted_acces)
-
-    res
-  end
 end
