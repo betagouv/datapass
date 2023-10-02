09e8bb64498607ab20f9eebad11e6b6a00986f98
Pierre-Eric Garcia <garciapierreeric@gmail.com>
2023-10-02 14:27:30 +0200

    Redirect to demarches simplifiées on API TP form routing (#1212)
    Co-authored-by: Pierre-Eric Garcia <garciapierreeric@gmil.com>

--- frontend/src/components/organisms/FormRouter.tsx
+++ frontend/src/components/organisms/FormRouter.tsx
@@ -5,14 +5,22 @@ import Loader from '../atoms/Loader';
 import Enrollment from '../templates/Enrollment';
 import { useFullDataProvider } from '../templates/hooks/use-full-data-provider';
 import NotFound from './NotFound';
+import ExternalRedirect from '../atoms/ExternalRedirect';
+import { TargetAPI } from '../../config/data-provider-configurations';
 
 const FormRouter = () => {
-  const { targetApi: targetApiFromUrl } = useParams();
+  const { targetApi: targetApiFromUrl, enrollmentId } = useParams();
   const targetApi = (targetApiFromUrl as string).replace(/-/g, '_');
 
   const { Component, configuration, notFound } = useFullDataProvider({
     targetApi: targetApi as string,
   });
+
+  if (targetApi === TargetAPI.api_tiers_de_prestation && !enrollmentId) {
+    return (
+      <ExternalRedirect url="https://www.demarches-simplifiees.fr/commencer/api-tiers-de-prestations" />
+    );
+  }
 
   if (notFound) {
     return <NotFound />;
