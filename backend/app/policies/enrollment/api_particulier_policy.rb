a4246d2a7d3cf2cb3ad950d41fd57da2616c7041
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-04 15:32:42 +0200

    Refactor policy configuration from file
    Introduce module which configure policies for enrollments with
    configuration from backend/config/data_providers.yml

--- backend/app/policies/enrollment/api_particulier_policy.rb
+++ backend/app/policies/enrollment/api_particulier_policy.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 class Enrollment::ApiParticulierPolicy < EnrollmentPolicy
+  include PolicyConfigurationFromFile
+
   def revoke?
     record.can_revoke_status? && (user.is_administrator? || user.is_instructor?(record.target_api))
-  end
-
-  def permitted_attributes
-    res = super
-
-    res.concat([
-      scopes: record.configuration["scopesConfiguration"].map { |scope| scope["value"].to_sym }
-    ])
-
-    res
   end
 end
