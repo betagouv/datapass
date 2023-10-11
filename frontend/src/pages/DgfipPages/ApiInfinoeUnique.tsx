d48e75ae445c86a9f9d41782779b3982ba3be769
Nicolas Schweisguth <n.schweisguth@gmail.com>
2023-10-11 10:19:38 +0200

    Update ApiInfinoeUnique.tsx

--- frontend/src/pages/DgfipPages/ApiInfinoeUnique.tsx
+++ frontend/src/pages/DgfipPages/ApiInfinoeUnique.tsx
@@ -55,10 +55,7 @@ const ApiInfinoeUnique = () => (
     <DemarcheSection />
     <DescriptionSection />
     <DonneesSection ScopesDescription={DataAreInTermsOfUseDescription} />
-    <CadreJuridiqueSection
-      defaultFondementJuridiqueTitle="Décret n°2012-1246 du 7 novembre 2012 relatif à la gestion budgétaire et comptable publique"
-      defaultFondementJuridiqueUrl="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000026597003/"
-    />
+    <CadreJuridiqueSection />
     <ÉquipeSection />
     <HomologationSecuriteSection />
     <VolumetrieSection />
