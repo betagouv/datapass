65d7a8a4a7c670961c4308bb0d425de7edc8fdce
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-17 14:28:48 +0200

    Simplify/bugfix how we export enrollments
    Previous version use an overkill approach with websockets, which does
    not work well with reverse proxy. Moreover, it is a little bit overkill
    to use this feature with so little data.
    
    In doubt, simplify.

--- backend/app/controllers/enrollments_live_controller.rb
+++ backend/app/controllers/enrollments_live_controller.rb
@@ -1,25 +1,11 @@
 class EnrollmentsLiveController < ApplicationController
-  include ActionController::Live
-
   before_action :authenticate_user!
 
   # GET /enrollments/export
   def export
     @enrollments = policy_scope(Enrollment)
 
-    # headers inspired from: https://piotrmurach.com/articles/streaming-large-zip-files-in-rails/
-    response.headers["Content-Disposition"] =
-      %(attachment; filename="export-datapass-#{Date.today}.csv")
-    response.headers["Content-Type"] = "text/csv; charset=utf-8"
-    response.headers.delete("Content-Length")
-    response.headers["Cache-Control"] = "no-cache"
-    response.headers["Last-Modified"] = Time.now.httpdate.to_s
-    response.headers["X-Accel-Buffering"] = "no"
-    response.stream.write CSV.generate_line(Enrollment.csv_attributes)
-    @enrollments.csv_collection.each do |line|
-      response.stream.write CSV.generate_line(line)
-    end
-  ensure
-    response.stream.close
+    send_data @enrollments.to_csv,
+      filename: "export-datapass-#{Date.today}.csv"
   end
 end
