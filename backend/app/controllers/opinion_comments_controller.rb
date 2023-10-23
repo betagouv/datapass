ca0634f1d0ad31e3066fc3b75c14facacdca6f0e
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-23 22:35:02 +0200

    Introduce create opinion comments endpoint

---
+++ backend/app/controllers/opinion_comments_controller.rb
@@ -0,0 +1,31 @@
+class OpinionCommentsController < AuthenticatedUserController
+  def create
+    authorize(opinion, :comment?)
+
+    organizer = CreateOpinionComment.call(
+      opinion: opinion,
+      current_user: current_user,
+      opinion_comment_params: opinion_comment_create_params
+    )
+
+    if organizer.success?
+      render json: opinion, status: :created
+    else
+      render json: organizer.opinion_comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def opinion
+    @opinion ||= enrollment.opinions.find(params[:opinion_id])
+  end
+
+  def enrollment
+    @enrollment ||= Enrollment.find(params[:enrollment_id])
+  end
+
+  def opinion_comment_create_params
+    params.require(:opinion_comment).permit(:content)
+  end
+end
--- backend/app/policies/opinion_policy.rb
+++ backend/app/policies/opinion_policy.rb
@@ -2,4 +2,11 @@ class OpinionPolicy < ApplicationPolicy
   def create?
     user.is_instructor?(record.target_api)
   end
+
+  def comment?
+    enrollment = record.enrollment
+
+    user.is_instructor?(enrollment.target_api) ||
+      user.is_reporter?(enrollment)
+  end
 end
