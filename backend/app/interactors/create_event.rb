092cc5cb06fee0d949ca81cd61885b649b611920
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-23 18:35:13 +0200

    Introduce Event opinion_created

--- backend/app/interactors/create_event.rb
+++ backend/app/interactors/create_event.rb
@@ -3,7 +3,16 @@ class CreateEvent < ApplicationInteractor
     context.event = Event.create!(
       enrollment: context.enrollment,
       user: context.current_user,
-      name: context.event_name
+      name: context.event_name,
+      entity: possible_entity
     )
+  end
+
+  private
+
+  def possible_entity
+    if context.event_name.start_with?("opinion_")
+      context.opinion
+    end
   end
 end
