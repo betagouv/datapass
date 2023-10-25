b71e16ee84f3dd5415b684cf3c6e0a50372d753c
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-25 16:56:03 +0200

    BUGFIX wrong serialization for webhooks
    Closes https://errors.data.gouv.fr/organizations/sentry/issues/5299/

--- backend/spec/serializers/webhook_serializer_spec.rb
+++ backend/spec/serializers/webhook_serializer_spec.rb
@@ -1,22 +1,22 @@
 require "rails_helper"
 
 RSpec.describe WebhookSerializer, type: :serializer do
+  subject { described_class.new(enrollment, event, extra_data) }
+
+  let(:enrollment) { create(:enrollment, :franceconnect, :complete) }
+  let(:enrollment_serialized) { WebhookEnrollmentSerializer.new(enrollment).serializable_hash }
+  let(:event) { "validated" }
+  let(:extra_data) { {comment: "its ok!"} }
+
+  before do
+    Timecop.freeze
+  end
+
+  after do
+    Timecop.return
+  end
+
   describe "#serializable_hash" do
-    subject { described_class.new(enrollment, event, extra_data) }
-
-    let(:enrollment) { create(:enrollment, :franceconnect, :complete) }
-    let(:enrollment_serialized) { WebhookEnrollmentSerializer.new(enrollment).serializable_hash }
-    let(:event) { "validated" }
-    let(:extra_data) { {comment: "its ok!"} }
-
-    before do
-      Timecop.freeze
-    end
-
-    after do
-      Timecop.return
-    end
-
     it do
       expect(subject.serializable_hash).to eq(
         {
@@ -27,6 +27,21 @@ RSpec.describe WebhookSerializer, type: :serializer do
             pass: enrollment_serialized
           }.merge(extra_data)
         }
+      )
+    end
+  end
+
+  describe "#to_json" do
+    it "renders serialized hash as json" do
+      expect(subject.to_json).to eq(
+        {
+          event: event,
+          fired_at: Time.now.to_i,
+          model_type: "Pass",
+          data: {
+            pass: enrollment_serialized
+          }.merge(extra_data)
+        }.to_json
       )
     end
   end
