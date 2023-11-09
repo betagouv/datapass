2307b884200a17af96a54d786003caa41732c2e2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-11-09 16:58:45 +0100

    Scout commit

--- backend/spec/organizers/create_opinion_comment_spec.rb
+++ backend/spec/organizers/create_opinion_comment_spec.rb
@@ -14,26 +14,28 @@ RSpec.describe CreateOpinionComment, type: :organizer do
   let(:enrollment) { create(:enrollment, :api_particulier) }
   let(:user) { create(:user, roles: ["api_particulier:reporter"]) }
 
-  it { is_expected.to be_success }
+  context "with valid params" do
+    it { is_expected.to be_success }
 
-  it "creates a new opinion comment from current user" do
-    expect { create_opinion_comment }.to change(OpinionComment, :count).by(1)
+    it "creates a new opinion comment from current user" do
+      expect { create_opinion_comment }.to change(OpinionComment, :count).by(1)
 
-    last_opinion_comment = OpinionComment.last
+      last_opinion_comment = OpinionComment.last
 
-    expect(last_opinion_comment.user).to eq(user)
-    expect(last_opinion_comment.opinion).to eq(opinion)
-    expect(last_opinion_comment.content).to eq("This is an advice")
-  end
+      expect(last_opinion_comment.user).to eq(user)
+      expect(last_opinion_comment.opinion).to eq(opinion)
+      expect(last_opinion_comment.content).to eq("This is an advice")
+    end
 
-  it "creates an event linked to this opinion comment" do
-    expect { create_opinion_comment }.to change(Event, :count).by(1)
+    it "creates an event linked to this opinion comment" do
+      expect { create_opinion_comment }.to change(Event, :count).by(1)
 
-    last_event = Event.last
+      last_event = Event.last
 
-    expect(last_event.name).to eq("opinion_comment_created")
-    expect(last_event.enrollment).to eq(enrollment)
-    expect(last_event.entity).to be_a(OpinionComment)
-    expect(last_event.user).to eq(user)
+      expect(last_event.name).to eq("opinion_comment_created")
+      expect(last_event.enrollment).to eq(enrollment)
+      expect(last_event.entity).to be_a(OpinionComment)
+      expect(last_event.user).to eq(user)
+    end
   end
 end
