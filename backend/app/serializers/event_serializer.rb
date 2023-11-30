610141b6e69b98dced5bfd7e9ac92320e535e183
Loïc Delmaire <loic.delmaire@gmail.com>
2023-11-30 16:10:34 +0100

    Scout commit

--- backend/app/serializers/event_serializer.rb
+++ backend/app/serializers/event_serializer.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class EventSerializer < ApplicationSerializer
-  attributes :id, :comment, :created_at, :diff, :name, :processed_at, :updated_at
+  attributes :id,
+    :comment,
+    :created_at,
+    :diff,
+    :name,
+    :processed_at,
+    :updated_at
 
   has_one :user
 end
