7aefdb90c613fd0c9dd13c8a059c73cfc135d28b
Isabelle Lafont <isalafont@gmail.com>
2023-12-12 16:43:11 +0100

    Refacto EnrollmentsLiveController according to feedbacks

--- backend/spec/controllers/enrollments/export_spec.rb
+++ backend/spec/controllers/enrollments/export_spec.rb
@@ -45,26 +45,7 @@ RSpec.describe EnrollmentsLiveController, "#export", type: :controller do
     it "should return an XLSX file name" do
       get :export, format: :xlsx
 
-      expect(response.headers["Content-Disposition"]).to include("attachment; filename=\"export DataPass-#{Date.today}.xlsx")
-    end
-  end
-
-  describe "payload" do
-    subject do
-      get :export
-    end
-
-    let(:user) { create(:instructor, target_api: "franceconnect") }
-    let!(:foreign_enrollment) { create_list(:enrollment, 10, :franceconnect) }
-
-    before do
-      login(user)
-    end
-
-    it "does not make database queries" do
-      pending "not find how to test it with xlsx instead of csv"
-
-      expect { subject }.to make_database_queries(count: 3)
+      expect(response.headers["Content-Disposition"]).to include("attachment; filename=\"export-datapass-#{Date.today}.xlsx")
     end
   end
 end
