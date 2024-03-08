dca03b72f3bc7a2ca65141cc0342606af55cfe5e
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-08 12:18:21 +0100

    EnrollmentsLiveController#export : speed up download by killing obvious N+1 queries

--- backend/app/controllers/enrollments_live_controller.rb
+++ backend/app/controllers/enrollments_live_controller.rb
@@ -1,6 +1,6 @@
 class EnrollmentsLiveController < AuthenticatedUserController
   def export
-    enrollments = policy_scope(Enrollment)
+    enrollments = policy_scope(Enrollment).includes(:events, :team_members)
 
     if enrollments.any?
       send_data SpreadsheetGenerator.new(enrollments).perform, filename: "export-datapass-#{Date.today}.xlsx", type: "application/xlsx"
