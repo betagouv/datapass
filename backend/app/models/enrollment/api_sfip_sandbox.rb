4d2929c16317537d7a967b9206ff7175b1bd14d0
Delmaire Loïc <loic.delmaire@gmail.com>
2024-03-15 16:02:08 +0100

    Enrollment::ApiSfipSandbox: validate incompatible scope (2nd try) (#1441)
    * Scout commit
    
    * Enrollment::ApiSfipSandbox: validate incompatible scope (2nd try)

--- backend/app/models/enrollment/api_sfip_sandbox.rb
+++ backend/app/models/enrollment/api_sfip_sandbox.rb
@@ -1,5 +1,11 @@
-# frozen_string_literal: true
-
 class Enrollment::ApiSfipSandbox < Enrollment::AbstractDgfipSandboxEnrollment
   include RequireRgpdGeneralAgreement
+
+  protected
+
+  def submit_validation
+    super
+
+    api_impot_particulier_scope_validation
+  end
 end
