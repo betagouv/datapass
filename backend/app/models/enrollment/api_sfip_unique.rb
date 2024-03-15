c808b678e2ebe7e0d9975a77a97793d7ef14aa14
Delmaire Loïc <loic.delmaire@gmail.com>
2024-03-15 17:01:29 +0100

    Enrollment::ApiSfipUnique: validate incompatible scopes (#1442)

--- backend/app/models/enrollment/api_sfip_unique.rb
+++ backend/app/models/enrollment/api_sfip_unique.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Enrollment::ApiSfipUnique < Enrollment::AbstractDgfipUniqueEnrollment
+  protected
+
+  def submit_validation
+    super
+
+    api_impot_particulier_scope_validation
+  end
 end
