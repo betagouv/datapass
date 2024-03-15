c24d63f2e2d64cab22e2a55afd92151e97ae9c17
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-15 14:54:00 +0000

    Enrollment::ApiSfipSandbox: validate incompatible scope (2nd try)

--- backend/app/models/enrollment/api_sfip_sandbox.rb
+++ backend/app/models/enrollment/api_sfip_sandbox.rb
@@ -1,3 +1,11 @@
 class Enrollment::ApiSfipSandbox < Enrollment::AbstractDgfipSandboxEnrollment
   include RequireRgpdGeneralAgreement
+
+  protected
+
+  def submit_validation
+    super
+
+    api_impot_particulier_scope_validation
+  end
 end
