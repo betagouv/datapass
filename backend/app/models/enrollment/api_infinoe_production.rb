0773c3fc2fa6ecff78bc585361d3ca372b77452c
Mehdi Farsi <mehdifarsi.pro@gmail.com>
2024-04-10 16:22:44 +0200

    remove homologation-related attributes in permitted params and validations

--- backend/app/models/enrollment/api_infinoe_production.rb
+++ backend/app/models/enrollment/api_infinoe_production.rb
@@ -1,2 +1,13 @@
 class Enrollment::ApiInfinoeProduction < Enrollment::AbstractDgfipProductionEnrollment
+  def production_form_validation
+    # Cadre juridique
+    cadre_juridique_validation
+
+    # Volumétrie
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la limitation d’appels par minute avant de continuer") unless additional_content&.fetch("volumetrie_appels_par_minute", false)&.present?
+
+    # CGU
+    errors.add(:cgu_approved, :invalid, message: "Vous devez valider les modalités d’utilisation avant de continuer") unless cgu_approved?
+    errors.add(:dpo_is_informed, :invalid, message: "Vous devez confirmer avoir informé le DPD de votre organisation avant de continuer") unless dpo_is_informed?
+  end
 end
--- backend/app/policies/enrollment/api_infinoe_production_policy.rb
+++ backend/app/policies/enrollment/api_infinoe_production_policy.rb
@@ -1,2 +1,27 @@
 class Enrollment::ApiInfinoeProductionPolicy < Enrollment::ProductionPolicy
+  def permitted_attributes
+    res = []
+
+    res.concat([
+      :cgu_approved,
+      :dpo_is_informed,
+      :target_api,
+      :previous_enrollment_id,
+      :fondement_juridique_title,
+      :fondement_juridique_url,
+      team_members_attributes: [:id, :type, :family_name, :given_name, :email, :phone_number, :job],
+      documents_attributes: [
+        :id,
+        :attachment,
+        :type,
+        :_destroy
+      ],
+      additional_content: [
+        :recette_fonctionnelle,
+        :volumetrie_appels_par_minute
+      ]
+    ])
+
+    res
+  end
 end
