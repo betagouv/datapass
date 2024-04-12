c0bc1734dbab3f1e9083813a07169ca605180860
Nicolas Schweisguth <n.schweisguth@gmail.com>
2024-04-12 17:51:28 +0200

    Modif doc API IP + ajout texte module

--- frontend/src/components/molecules/ImpotPartDocumentationAlert/index.tsx
+++ frontend/src/components/molecules/ImpotPartDocumentationAlert/index.tsx
@@ -7,14 +7,18 @@ export const ImpotPartDocumentationAlert: React.FC = () => (
     <div style={{ width: '70%', marginBottom: '20px' }}>
       Afin de vous accompagner dans le choix de vos données, vous pouvez
       consulter notre documentation qui vous permettra de comprendre le
-      fonctionnement de notre API ainsi que les données que nous proposons.
+      fonctionnement de notre API ainsi que les données que nous proposons. Afin
+      de respecter les principes de collecte, traitement et conservation données
+      définit par l’article 4 de la loi informatique et libertés, vous devez
+      devez sélectionner uniquement les données strictement nécessaires à votre
+      téléservice.
     </div>
     <Button
       icon="file"
       iconRight
       target="_blank"
       rel="noreferrer noopener"
-      href="https://datapass.api.gouv.fr/docs/presentation_de_l_api_impot_particulier_v2.6.pdf"
+      href="https://datapass.api.gouv.fr/docs/presentation_de_l_api_impot_particulier_v2.7.pdf"
     >
       Voir la documentation de l’API Impôt particulier
     </Button>
