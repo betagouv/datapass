f2d703d23a80f5bf2501437c3e9e71e924f7ebbf
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-05-29 10:38:05 +0200

    Change rgpd additionnal terms of use without homologation for Api Infinoe sandbox only

--- frontend/src/pages/DgfipPages/common.tsx
+++ frontend/src/pages/DgfipPages/common.tsx
@@ -23,6 +23,19 @@ export const additionalTermsOfUse = [
   },
 ];
 
+export const additionalTermsOfUseWithoutHomologation = [
+  {
+    id: 'rgpd_general_agreement',
+    label: (
+      <>
+        J’atteste que mon organisation devra déclarer à la DGFiP
+        l’accomplissement des formalités en matière de protection des données à
+        caractère personnel.
+      </>
+    ),
+  },
+];
+
 export const DonneesDescription = () => (
   <>
     <p>
