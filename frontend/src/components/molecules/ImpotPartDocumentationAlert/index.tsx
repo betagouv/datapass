03dc45a966d65063360990cc5f9378eeb91dec84
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-05-29 11:17:54 +0200

    Add a style sheet to clean code

--- frontend/src/components/molecules/ImpotPartDocumentationAlert/index.tsx
+++ frontend/src/components/molecules/ImpotPartDocumentationAlert/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Highlight from '../../atoms/Highlight';
 import Button from '../../atoms/hyperTexts/Button';
+import './style.css';
 
 export const ImpotPartDocumentationAlert: React.FC = () => (
   <Highlight title="Mode de fonctionnement de l’API Impôt particulier">
-    <div style={{ width: '70%', marginBottom: '20px' }}>
+    <div className="impot-part-doc-alert">
       Afin de vous accompagner dans le choix de vos données, vous pouvez
       consulter notre documentation qui vous permettra de comprendre le
       fonctionnement de notre API ainsi que les données que nous proposons. Afin
