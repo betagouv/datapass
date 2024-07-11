e4b7df34e9ef03e0e0d25918fff15d67ffdcbda3
Isabelle Lafont <43042737+Isalafont@users.noreply.github.com>
2024-07-11 14:53:34 +0200

    Fix/dat 457 dgfip access mode (#1516)
    * Re install dgfip validation access_mode and test
    
    * Revert Frontend to display access_mode question

--- backend/app/models/enrollment/api_impot_particulier_unique.rb
+++ backend/app/models/enrollment/api_impot_particulier_unique.rb
@@ -7,5 +7,6 @@ class Enrollment::ApiImpotParticulierUnique < Enrollment::AbstractDgfipUniqueEnr
     super
 
     api_impot_particulier_scope_validation
+    api_impot_particulier_acces_validation
   end
 end
---
+++ backend/spec/models/enrollment/api_impot_particulier_sandbox_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe Enrollment::ApiImpotParticulierSandbox, type: :model do
+  let(:user) { create(:user) }
+  let(:enrollment) { create(:enrollment, :api_impot_particulier_sandbox, :draft) }
+
+  describe "#api_impot_particulier_scope_validation" do
+    context "when no scope is selected and no specific requirement is expressed" do
+      it "is not valid" do
+        enrollment.send(:submit_validation)
+        expect(enrollment.errors[:scopes]).to include("Vous devez cocher au moins une modalité d’accès avant de continuer")
+      end
+    end
+
+    context "when no scope is selected but a specific requirement is expressed" do
+      before { enrollment.update(additional_content: {"specific_requirements" => true}) }
+
+      it "is valid" do
+        enrollment.send(:submit_validation)
+        expect(enrollment.errors[:scopes]).not_to include("Vous devez cocher au moins une année de revenus souhaitée avant de continuer")
+      end
+    end
+  end
+end
