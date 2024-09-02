ff28332494a2f8231fcfdbc114f5e6850741e292
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-09-02 16:05:51 +0200

    Fix test for api_impot_particulier_sandbox

--- backend/spec/models/enrollment/api_impot_particulier_sandbox_spec.rb
+++ backend/spec/models/enrollment/api_impot_particulier_sandbox_spec.rb
@@ -2,13 +2,13 @@
 
 RSpec.describe Enrollment::ApiImpotParticulierSandbox, type: :model do
   let(:user) { create(:user) }
-  let(:enrollment) { create(:enrollment, :api_impot_particulier_sandbox, :draft) }
+  let(:enrollment) { create(:enrollment, :api_impot_particulier_sandbox, :draft, scopes: []) }
 
   describe "#api_impot_particulier_scope_validation" do
     context "when no scope is selected and no specific requirement is expressed" do
       it "is not valid" do
         enrollment.send(:submit_validation)
-        expect(enrollment.errors[:scopes]).to include("Vous devez cocher au moins une modalité d’accès avant de continuer")
+        expect(enrollment.errors[:scopes]).to include("Vous devez cocher au moins une année de revenus souhaitée avant de continuer")
       end
     end
 
