0c88316312a0810c15d9e6fe8fc6bca7114c3981
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-09-03 10:51:32 +0200

    Add same rules on api_impot part fc sandbox & unique.

--- backend/app/policies/enrollment/api_impot_particulier_fc_unique_policy.rb
+++ backend/app/policies/enrollment/api_impot_particulier_fc_unique_policy.rb
@@ -5,8 +5,9 @@ class Enrollment::ApiImpotParticulierFcUniquePolicy < Enrollment::UniquePolicy
     res = super
 
     res.concat([
-      scopes: impot_particulier_fc_permitted_scopes
+      scopes: impot_particulier_permitted_scopes
     ])
+    augment_permitted_attributes(res, :additional_content, *impot_particulier_permitted_acces, *impot_particulier_specific_requirements)
 
     res
   end
