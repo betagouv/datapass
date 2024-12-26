98cb07be521d035f4fc9b7f072d216c40bca5681
Isabelle Lafont <isabelle.lafont@beta.gouv.fr>
2024-12-26 15:36:19 +0100

    Add Access mode in frontend
      - and Add Demarches but demarches still not displayed

--- frontend/src/pages/DgfipPages/api-rial-sandbox.tsx
+++ frontend/src/pages/DgfipPages/api-rial-sandbox.tsx
@@ -10,13 +10,69 @@ import DonneesSection from '../../components/organisms/form-sections/DonneesSect
 import { DATA_PROVIDER_CONFIGURATIONS } from '../../config/data-provider-configurations';
 import PreviousEnrollmentSection from '../../components/organisms/form-sections/PreviousEnrollmentSection';
 import ApiRialDocumentationAlert from '../../components/molecules/ApiRialDocumentationAlert';
+import { accessModes } from './api-impot-particulier-common';
 
 const target_api = 'api_rial_sandbox';
 const steps = [target_api, 'api_rial_production'];
 
+export const demarches = {
+  default: {
+    label: 'Demande libre',
+    state: {
+      intitule: '',
+      description: '',
+      data_recipients: '',
+      fondement_juridique_title: '',
+    },
+  },
+  recherche_local_identifiant: {
+    label: 'Recherche d’un local par son identifiant fiscal',
+    state: {
+      intitule: 'Recherche d’un local par son identifiant fiscal',
+      description: '',
+      data_recipients: '',
+      fondement_juridique_title: '',
+    },
+  },
+  recherche_locaux_parcelle_cadastre: {
+    label: 'Recherche de locaux par une parcelle cadastrale',
+    state: {
+      intitule: 'Recherche de locaux par une parcelle cadastrale',
+      description: '',
+      data_recipients: '',
+      fondement_juridique_title: '',
+    },
+  },
+  recherche_locaux_adresse: {
+    label: 'Recherche de locaux par une adresse',
+    state: {
+      intitule: 'Recherche de locaux par une adresse',
+      description: '',
+      data_recipients: '',
+      fondement_juridique_title: '',
+    },
+  },
+};
+
+export const accessModes = [
+  {
+    id: 'recherche_local_identifiant',
+    label: 'Recherche d’un local par son identifiant fiscal',
+  },
+  {
+    id: 'recherche_locaux_parcelle_cadastre',
+    label: 'Recherche de locaux par une parcelle cadastrale',
+  },
+  {
+    id: 'recherche_locaux_adresse',
+    label: 'Recherche de locaux par une adresse',
+  },
+];
+
 const ApiRialSandbox = () => (
   <Form
     target_api={target_api}
+    demarches={demarches}
     contactEmail={DATA_PROVIDER_CONFIGURATIONS[target_api]?.email}
     documentationUrl="https://api.gouv.fr/producteurs/dgfip"
   >
