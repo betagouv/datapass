f857a61df8ea1f9ae836fbead9838029cdd8cb5f
Isabelle Lafont <43042737+Isalafont@users.noreply.github.com>
2024-12-27 12:25:55 +0100

    Revert "Feature/dat 768 api rial ajout de cas dusage"

--- backend/app/models/concerns/dgfip_validation_methods.rb
+++ backend/app/models/concerns/dgfip_validation_methods.rb
@@ -10,8 +10,7 @@ module DgfipValidationMethods
   def rgpd_general_agreement_validation
     return if additional_content&.fetch("rgpd_general_agreement", false)
 
-    errors.add(:additional_content, :invalid,
-      message: "Vous devez attester que votre organisation déclarera à la DGFiP l'accomplissement des formalités en terme de protection des données")
+    errors.add(:additional_content, :invalid, message: "Vous devez attester que votre organisation déclarera à la DGFiP l'accomplissement des formalités en terme de protection des données")
   end
 
   def api_impot_particulier_scope_validation
@@ -35,56 +34,24 @@ module DgfipValidationMethods
     errors.add(:scopes, :invalid, message: "Vous devez cocher au moins une modalité d’accès avant de continuer")
   end
 
-  def api_rial_access_validation
-    return if additional_content.any? do |k, v|
-      v && %w[recherche_local_identifiant recherche_locaux_parcelle_cadastre recherche_locaux_adresse].include?(k)
-    end
-
-    errors.add(:scopes, :invalid, message: "Vous devez cocher au moins une modalité d’accès avant de continuer")
-  end
-
   def production_form_validation
     # Cadre juridique
     cadre_juridique_validation
 
     # Homologation de securite
-    unless additional_content&.fetch("autorite_homologation_nom", false)&.present?
-      errors.add(:additional_content, :invalid,
-        message: "Vous devez renseigner le nom de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer")
-    end
-    unless additional_content&.fetch("autorite_homologation_fonction", false)&.present?
-      errors.add(:additional_content, :invalid,
-        message: "Vous devez renseigner la fonction de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer")
-    end
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner le nom de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer") unless additional_content&.fetch("autorite_homologation_nom", false)&.present?
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la fonction de l’autorité d’homologation ou du signataire du questionnaire de sécurité avant de continuer") unless additional_content&.fetch("autorite_homologation_fonction", false)&.present?
     date_regex = /^\d{4}-\d{2}-\d{2}$/
-    unless date_regex.match?(additional_content&.fetch("date_homologation", ""))
-      errors.add(:additional_content, :invalid,
-        message: "Vous devez renseigner la date de début de l’homologation ou de signature du questionnaire de sécurité au format AAAA-MM-JJ avant de continuer")
-    end
-    unless date_regex.match?(additional_content&.fetch("date_fin_homologation", ""))
-      errors.add(:additional_content, :invalid,
-        message: "Vous devez renseigner la date de fin de l’homologation au format AAAA-MM-JJ avant de continuer")
-    end
-    unless documents.where(type: "Document::DecisionHomologation").present?
-      errors.add(:documents, :invalid,
-        message: "Vous devez joindre le document de décision d’homologation ou le questionnaire de sécurité avant de continuer")
-    end
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de début de l’homologation ou de signature du questionnaire de sécurité au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_homologation", ""))
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la date de fin de l’homologation au format AAAA-MM-JJ avant de continuer") unless date_regex.match?(additional_content&.fetch("date_fin_homologation", ""))
+    errors.add(:documents, :invalid, message: "Vous devez joindre le document de décision d’homologation ou le questionnaire de sécurité avant de continuer") unless documents.where(type: "Document::DecisionHomologation").present?
 
     # Volumétrie
-    unless additional_content&.fetch("volumetrie_appels_par_minute", false)&.present?
-      errors.add(:additional_content, :invalid,
-        message: "Vous devez renseigner la limitation d’appels par minute avant de continuer")
-    end
+    errors.add(:additional_content, :invalid, message: "Vous devez renseigner la limitation d’appels par minute avant de continuer") unless additional_content&.fetch("volumetrie_appels_par_minute", false)&.present?
 
     # CGU
-    unless cgu_approved?
-      errors.add(:cgu_approved, :invalid,
-        message: "Vous devez valider les modalités d’utilisation avant de continuer")
-    end
-    unless dpo_is_informed?
-      errors.add(:dpo_is_informed, :invalid,
-        message: "Vous devez confirmer avoir informé le DPD de votre organisation avant de continuer")
-    end
+    errors.add(:cgu_approved, :invalid, message: "Vous devez valider les modalités d’utilisation avant de continuer") unless cgu_approved?
+    errors.add(:dpo_is_informed, :invalid, message: "Vous devez confirmer avoir informé le DPD de votre organisation avant de continuer") unless dpo_is_informed?
   end
 
   private
@@ -97,8 +64,7 @@ module DgfipValidationMethods
 
     return unless (scopes & selected_years).empty?
 
-    errors.add(:scopes, :invalid,
-      message: "Vous devez cocher au moins une année de revenus souhaitée avant de continuer")
+    errors.add(:scopes, :invalid, message: "Vous devez cocher au moins une année de revenus souhaitée avant de continuer")
   end
 
   def validate_incompatible_scopes(incompatible_scopes)
@@ -108,15 +74,13 @@ module DgfipValidationMethods
 
     return unless (scopes & special_scopes).present? && (scopes & incompatible_scopes).present?
 
-    errors.add(:scopes, :invalid,
-      message: "Des données incompatibles entre elles ont été cochées. Pour connaître les modalités d’appel et de réponse de l’API Impôt particulier ainsi que les données proposées, vous pouvez consulter le guide de présentation de cette API dans la rubrique « Les données nécessaires > Comment choisir les données »")
+    errors.add(:scopes, :invalid, message: "Des données incompatibles entre elles ont été cochées. Pour connaître les modalités d’appel et de réponse de l’API Impôt particulier ainsi que les données proposées, vous pouvez consulter le guide de présentation de cette API dans la rubrique « Les données nécessaires > Comment choisir les données »")
   end
 
   def validate_exclusive_years_scope_combination
     if (scopes & %w[dgfip_annee_n_moins_2_si_indispo_n_moins_1]).present? &&
         (scopes & %w[dgfip_annee_n_moins_1 dgfip_annee_n_moins_2 dgfip_annee_n_moins_3]).present?
-      errors.add(:scopes, :invalid,
-        message: "Vous ne pouvez pas sélectionner la donnée 'avant dernière année de revenu, si la dernière année de revenu est indisponible' avec d'autres années de revenus")
+      errors.add(:scopes, :invalid, message: "Vous ne pouvez pas sélectionner la donnée 'avant dernière année de revenu, si la dernière année de revenu est indisponible' avec d'autres années de revenus")
     end
   end
 end
--- backend/app/models/enrollment/api_rial_sandbox.rb
+++ backend/app/models/enrollment/api_rial_sandbox.rb
@@ -1,11 +1,3 @@
 class Enrollment::ApiRialSandbox < Enrollment::AbstractDgfipSandboxEnrollment
   include RequireRgpdGeneralAgreement
-
-  protected
-
-  def submit_validation
-    super
-
-    api_rial_access_validation
-  end
 end
